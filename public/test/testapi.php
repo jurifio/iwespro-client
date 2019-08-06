02cb44fae6ca07290da02a3f89b3fbfb59f99e54
juri fiorani <jurif@hotmail.com>
2019-08-06 19:19:35 +0200

    allineamento ean

--- public/test/testapi.php
+++ public/test/testapi.php
@@ -32,40 +32,70 @@ $time = microtime(true);
 
 $stringApi= '[
 {
-"referenceId": "349900071795",
-"supplierArticle": "AFS023",
+"referenceId": "349900071797",
+"supplierArticle": "AFS02354",
 "brand": "ALBERTO FERMANI",
 "categories": ["Donna", "Calzature", "Stivali"],
 "year": 2019,
 "season": "Autunno-Inverno 19",
-"var": "ROSSO",
+"var": "BLU",
 "size": "36",
 "qty": 2,
 "supplierPrice": 200.0,
 "marketPrice": 300.0,
 "imgs": ["https://eversellstorage.blob.core.windows.net/eversell-images-demo/9bda9a31-a845-42d9-922b-5a8dc09642d0.png", "https://eversellstorage.blob.core.windows.net/eversell-images-demo/b5f94394-a7f9-448b-8243-3460eb9a9a29.png", "https://eversellstorage.blob.core.windows.net/eversell-images-demo/e8e7f3f8-0f2b-4746-899c-2942686528b7.png"],
-"barcodeInt": "012345678901",
-"ean": "012345678901",
+"barcodeInt": "012345678911",
+"ean": "012345678911",
 "tags": ["stivaletto","calzature","stivaletto donna"],
 "sizeGroup": "calzature",
 "audience": "donna",
-"colorDescription": "ROSSO",
-"name": "Stivaletto donna rosso",
-"description": "test"
+"colorDescription": "BLU",
+"name": "Stivaletto donna blu",
+"description": "test2"
 }
 ]';
-$urlInsert = "https://api.fattureincloud.it/v1/fatture/nuovo";
-$options = array(
+$urlInsert = "https://stage.iwes.pro/api/products?id=2&password=Iwesiwesiwes19!";
+/*$options = array(
     "http" => array(
         "header" => "Content-type: text/json\r\n",
         "method" => "POST",
         "content" => $stringApi
     ),
-);
-$context = stream_context_create($options);
-$result = json_decode(file_get_contents($urlInsert, false, $context), true);
-if (array_key_exists('success', $result)) {
-    $resultApi = "Risultato=" . $result['success'] . " new_id:" . $result['new_id'] . " token:" . $result['token'];
+);*/
+if (ENV == 'dev') {
+    $pathlocal = '/media/sf_sites/iwespro/temp-json/';
+    $save_to = '/media/sf_sites/iwespro/temp-json/';
+    $save_to_dir = '/media/sf_sites/iwespro/temp-json/';
+
 } else {
-    $resultApi = "Errore=" . $result['error'] . " codice di errore:" . $result['error_code'];
+    $pathlocal = '/home/iwespro/public_html/temp-json/';
+    $save_to = '/home/iwespro/public_html/temp-json/';
+    $save_to_dir = '/home/iwespro/public_html/temp-json/';
+
 }
+
+
+
+//Initiate cURL.
+$ch = curl_init($urlInsert);
+
+//The JSON data.
+$jsonData = array(
+    'username' => 'MyUsername',
+    'password' => 'MyPassword'
+);
+
+//Encode the array into JSON.
+//$jsonDataEncoded = json_encode($jsonData);
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $stringApi);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+//Execute the request
+$result = curl_exec($ch);
