a935c6a393f0078ecb15e649c6bf16ce9fae7241
juri fiorani <jurif@hotmail.com>
2019-09-20 11:52:14 +0200

    allineamento ordini pagamenti carte e paypal

--- public/content/mail/friendorderreminder/friendorderreminder.php
+++ public/content/mail/friendorderreminder/friendorderreminder.php
@@ -8,35 +8,88 @@ use bamboo\core\theming\CMailerHelper;
 </head>
 <body>
 Ciao,<br>
-    ti ricordiamo che devi confermare <?php echo count($orderLines) ?> ordini.<br />
+    ti ricordiamo che devi confermare <?php echo count($lines) ?> ordini.<br />
     Ti prego di preparare i prodotti per la spedizione <br />
 
-    <br />
-    <table>
-        <thead>
-            <tr>
-                <th>Numero Ordine</th>
-            </tr>
-        </thead>
-        <tbody>
+
+<br />
+<table>
+    <?php
+    $extId = isset($lines[0]['extId']);
+    ?>
+    <thead>
+    <tr>
+        <th>Ordine-Riga</th>
+        <th>Nome Prodotto</th>
         <?php
-            foreach($orderLines as $orderLine) {
-            echo '<tr>';
-                echo '<td>'.$orderLine->printId().'</td>';
-            echo '</tr>';
-            }
-        ?>
-        </tbody>
+        if($extId) {
+            echo '<th>Codice Friend</th>';
+        } ?>
+        <th>Codice Prodotto Fornitore</th>
+        <th>Colore</th>
+        <th>Brand</th>
+        <th>Taglia</th>
+        <th>Prezzo Friend</th>
+        <th>Anteprima</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    $checkParallal=[];
+    foreach($lines as $line){
+        echo '<tr>';
+        echo '<td>'.$line['orderId'].'-'.$line['orderLineId'].'</td>';
+        echo '<td>'.$line['productNameTranslation'].'</td>';
+        if($extId) {
+            echo '<td>'; echo $line['extId']; echo '</td>';
+        }
+        echo '<td>'; echo $line['itemno']; echo '</td>';
+        echo '<td>'; echo $line['var']; echo '</td>';
+        echo '<td>'; echo $line['brand']; echo '</td>';
+        echo '<td>'; echo $line['size']; echo '</td>';
+        echo '<td>'; echo $line['friendRevenue']; echo '</td>';
+        echo '<td><img height="70" src="'.$app->image($line['photo'],'amazon').'"></td>';
+        echo '</tr>';
+        array_push($checkParallal,$line['shopId']);
+        $order=$orderRepo->findOneBy(['id'=>$line['orderId']]);
+    }
+    $checkOrigin=[];
+    foreach($checkParallal as $check){
+        if ($check==$order->remoteShopSellerId) {
+            array_push($checkOrigin, '1');
+        }else{
+            array_push($checkOrigin, '0');
+        }
 
+    }
+    $address=json_decode($order->frozenShippingAddress,true);
+    $findCountry=\Monkey::app()->repoFactory->create('Country')->findOneBy(['id'=>$address['countryId']]);
+    if($findCountry!=null){
+        $country=$findCountry->name;
+    }else{
+        $country='';
+    }
+    ?>
+    </tbody>
+</table>
+<br>
+<br>
+Indirizzo di spedizione del prodotto:<br>
+<br>
+<p style="font-weight:bold"><?php echo $address['name'].' '.$address['surname'];?><br>
+    <?php echo $address['Company'];?><br></p>
+<?php echo $address['address'];?><br>
+<?php echo $address['postcode'].' ' .$address['city'].' '.$address['province']?><br>
+<?php echo $country;?><br>
+<br>
+<?php   if (in_array('0', $checkOrigin, true)){
+    "<p style='font-weight:bold'>Prego utilizzare un nastro neutro per il confezionamento del pacco<br></p>";
+}
+?>
+<p style="font-weight:bold">In caso di domande, richieste o suggerimenti, non esitate a contattarci tramite telefono al seguente numero 0733-471365 o via e-mail all'indirizzo friends@iwes.it<br></p>
 
-    </table>
-    puoi confermare gli ordini seguendo il seguente link: <br />
-    <a href="https://www.iwes.pro/blueseal/friend/ordini">https://www.iwes.pro/blueseal/friend/ordini</a><br />
-    la mancata conferma tempestiva comporterà il ritardo di un giorno<br />
-    Saluti<br />
-
-    <img src="https://www.pickyshop.com/it/assets/logowide.png"><br />
-    Friends Support
-
+Saluti<br>
+<img src="https://www.pickyshop.com/it/assets/logowide.png"><br>
+Friends Support
 </body>
 </html>
