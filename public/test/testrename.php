5708d41eab1fcc7d56e1954aa592f3382f6a7063
juri fiorani <jurif@hotmail.com>
2019-10-15 17:24:17 +0200

    creazione Vista Vendite

---
+++ public/test/testrename.php
@@ -0,0 +1,50 @@
+<?php
+ini_set("memory_limit", "2000M");
+ini_set('max_execution_time', 0);
+$ttime = microtime(true);
+$time = microtime(true);
+require '../../iwesStatic.php';
+var_dump('Applicazione  \t\t\t\t' . (microtime(true) - $time));
+
+$monkey = \Monkey::app();
+$time = microtime(true);
+$monkey->dbAdapter;
+var_dump("dbAdapter \t\t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->cacheService;
+var_dump("cacheService \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->sessionManager;
+var_dump("sessionManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->authManager;
+var_dump("authManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->entityManagerFactory;
+var_dump("entityManagerFactory \t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->repoFactory;
+var_dump("repoFactory \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->eventManager;
+var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$filenametoextrat="110071-5424840__fila - 1010302 - rose__90201900000320003.jpg";
+echo $filenametoextrat.'<br/>';
+$firstPosition = strpos($filenametoextrat, '__');
+echo $firstPosition."<br />";
+$lastPosition=strpos($filenametoextrat,substr(strrchr($filenametoextrat, '__'),0));
+echo $item="1";
+echo "<br/>";
+echo $lastPosition;
+$firsPart =substr($filenametoextrat,0,$firstPosition);
+echo "<br>".$firsPart;
+echo "<br/>";
+    $secondPart=substr($filenametoextrat,$firstPosition,$lastPosition-$firstPosition+13);
+
+    echo $prefinalName=$firsPart.'__Barbagalloshop'.$secondPart.'_00'.$item.'.jpg';
+  /*echo  "<br>".str_replace('.jpg','__00'.$item.'.jpg',$prefinalName);
+
+
+
+
