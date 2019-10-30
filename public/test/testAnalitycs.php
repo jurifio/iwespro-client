8d01ed070c9e108a192fcd6c832ed45ed76896be
juri fiorani <jurif@hotmail.com>
2019-10-30 20:14:56 +0100

    creazione Vista Vendite

---
+++ public/test/testAnalitycs.php
@@ -0,0 +1,43 @@
+<?php
+ini_set("memory_limit", "2000M");
+ini_set('max_execution_time', 0);
+$ttime = microtime(true);
+$time = microtime(true);
+require '../../iwesStatic.php';
+var_dump('Applicazione  \t\t\t\t' . (microtime(true) - $time));
+
+$monkey = \Monkey::app();
+$time = microtime(true);
+$monkey->dbAdapter;
+var_dump("dbAdapter \t\t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->cacheService;
+var_dump("cacheService \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->sessionManager;
+var_dump("sessionManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->authManager;
+var_dump("authManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->entityManagerFactory;
+var_dump("entityManagerFactory \t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->repoFactory;
+var_dump("repoFactory \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->eventManager;
+var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+\Monkey::app()->vendorLibraries->load("analytics");
+$client = new Google_Client();
+$client->setApplicationName("Client_Library_Examples");
+$client->setDeveloperKey("7246359a0c308b26b7fec34129b4298de81f0fdd");
+
+$service = new Google_Service_Books($client);
+$optParams = array('filter' => 'free-ebooks');
+$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+
+foreach ($results as $item) {
+    echo $item['volumeInfo']['title'], "<br /> \n";
+}
