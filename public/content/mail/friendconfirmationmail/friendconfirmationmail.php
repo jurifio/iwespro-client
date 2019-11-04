34b0fc2624154b3383115078d28c1c68857ab9f3
juri fiorani <jurif@hotmail.com>
2019-11-04 20:38:52 +0000

    Update friendconfirmationmail.php

--- public/content/mail/friendconfirmationmail/friendconfirmationmail.php
+++ public/content/mail/friendconfirmationmail/friendconfirmationmail.php
@@ -9,56 +9,56 @@ use bamboo\core\theming\CMailerHelper;
 <body>
 
 Ciao,<br>
-    Ho il piacere di comunicarTi che il Tuo oggetto è stato venduto.<br>
-    Ti prego di <a href="https://www.iwes.pro/blueseal/friend/ordini" target="_blank">confermare le righe dell'ordine</a> (previo login) e preparare il pacco per la spedizione.<br>
+Ho il piacere di comunicarTi che il Tuo oggetto è stato venduto.<br>
+Ti prego di <a href="https://www.iwes.pro/blueseal/friend/ordini" target="_blank">confermare le righe dell'ordine</a> (previo login) e preparare il pacco per la spedizione.<br>
 
-    <br>
-    <table>
-        <?php
-        $extId = isset($lines[0]['extId']);
+<br>
+<table>
+    <?php
+    $extId = isset($lines[0]['extId']);
 
-        echo '<head>';
-        echo '<th>Ordine-Riga</th>';
-        echo '<th>Nome Prodotto</th>';
+    echo '<head>';
+    echo '<th>Ordine-Riga</th>';
+    echo '<th>Nome Prodotto</th>';
+    if($extId) {
+        echo '<th>Codice Friend</th>';
+    }
+    echo '<th>Codice Prodotto Fornitore</th>';
+    echo '<th>Colore</th>';
+    echo '<th>Brand</th>';
+    echo '<th>Taglia</th>';
+    echo '<th>Prezzo Friend</th>';
+    echo '<th>Anteprima</th>';
+    echo '<th>Indirizzo di Spedizione</th>';
+    echo '<th>Tipologia di Nastro</th>';
+    echo '<th>Azione</th>';
+    $checkParallal=[];
+    $orderRepo=\Monkey::app()->repoFactory->create('Order');
+    foreach($lines as $line){
+        echo '<tr>';
+        echo '<td>'.$line['orderId'].'-'.$line['orderLineId'].'</td>';
+        echo '<td>'.$line['productNameTranslation'].'</td>';
         if($extId) {
-            echo '<th>Codice Friend</th>';
+            echo '<td>'; echo $line['extId']; echo '</td>';
         }
-        echo '<th>Codice Prodotto Fornitore</th>';
-        echo '<th>Colore</th>';
-        echo '<th>Brand</th>';
-        echo '<th>Taglia</th>';
-        echo '<th>Prezzo Friend</th>';
-        echo '<th>Anteprima</th>';
-        echo '<th>Indirizzo di Spedizione</th>';
-        echo '<th>Tipologia di Nastro</th>';
-        echo '<th>Azione</th>';
-        $checkParallal=[];
-        $orderRepo=\Monkey::app()->repoFactory->create('Order');
-        foreach($lines as $line){
-            echo '<tr>';
-            echo '<td>'.$line['orderId'].'-'.$line['orderLineId'].'</td>';
-            echo '<td>'.$line['productNameTranslation'].'</td>';
-            if($extId) {
-                echo '<td>'; echo $line['extId']; echo '</td>';
+        echo '<td>'; echo $line['itemno']; echo '</td>';
+        echo '<td>'; echo $line['var']; echo '</td>';
+        echo '<td>'; echo $line['brand']; echo '</td>';
+        echo '<td>'; echo $line['size']; echo '</td>';
+        echo '<td>'; echo $line['friendRevenue']; echo '</td>';
+        echo '<td><img height="70" src="'.$app->image($line['photo'],'amazon').'"></td>';
+        array_push($checkParallal,$line['shopId']);
+        $order=$orderRepo->findOneBy(['id'=>$line['orderId']]);
+        $checkOrigin=[];
+        foreach($checkParallal as $check){
+            if ($check==$order->remoteShopSellerId) {
+                array_push($checkOrigin, '1');
+            }else{
+                array_push($checkOrigin, '0');
             }
-            echo '<td>'; echo $line['itemno']; echo '</td>';
-            echo '<td>'; echo $line['var']; echo '</td>';
-            echo '<td>'; echo $line['brand']; echo '</td>';
-            echo '<td>'; echo $line['size']; echo '</td>';
-            echo '<td>'; echo $line['friendRevenue']; echo '</td>';
-            echo '<td><img height="70" src="'.$app->image($line['photo'],'amazon').'"></td>';
-            array_push($checkParallal,$line['shopId']);
-            $order=$orderRepo->findOneBy(['id'=>$line['orderId']]);
-            $checkOrigin=[];
-            foreach($checkParallal as $check){
-                if ($check==$order->remoteShopSellerId) {
-                    array_push($checkOrigin, '1');
-                }else{
-                    array_push($checkOrigin, '0');
-                }
 
-            }
-            echo '<td>';
+        }
+        echo '<td>';
         $address=json_decode($order->frozenShippingAddress,true);
         $findCountry=\Monkey::app()->repoFactory->create('Country')->findOneBy(['id'=>$address['countryId']]);
         if($findCountry!=null){
@@ -66,65 +66,66 @@ Ciao,<br>
         }else{
             $country='';
         }
-            ?>
-            <?php if($order->isShippingToIwes==null) { ?>
-                <br>
-                <p style="font-weight:bold"><?php echo $address['name'] . ' ' . $address['surname']; ?><br>
-                    <?php if (isset ($address['Company'])) {
-                        echo $address['Company'] . '<br>';
-                    }
-                    ?>
-                </p>
-                <?php echo $address['address']; ?><br>
-                <?php echo $address['postcode'] . ' ' . $address['city'] . ' ' . $address['province'] ?><br>
-                <?php echo $country; ?><br>
-                <br>
-                <?php echo '</td>';
-            }else{?>
-                <br>
-                <p style="font-weight:bold">Iwes  International Web Ecommerce Services snc<br>
-                </p>
-                via Cesare Pavese , 1 <br>
-                62012 Civitanova Marche (MC)<br>
-                ITALIA<br>
-                <br>
-                </td>
-            <? }
-               echo '<td>';
-            if (in_array('0', $checkOrigin, true)) {
-                "<p style='font-weight:bold'>Prego utilizzare un nastro neutro per il confezionamento del pacco<br></p>";
-            }
-            echo '</td>';
-            echo '<td>';
-            if($line['remoteShopSellerId']!=44) {
-                $findShopParallel = \Monkey::app()->repoFactory->create('Shop')->findOneBy(['id' => $line['shopId']]);
-                if ($findShopParallel->hasEcommerce == 1 && in_array('0',$checkOrigin,true)) {
-                    echo '<a href="' . $findShopParallel->urlSite . '/blueseal/friend/ordini" target="_blank">confermare la riga </a>';
-                } else {
-                    echo '<a href="https://www.iwes.pro/blueseal/friend/ordini" target="_blank">confermare la riga </a>';
+        ?>
+        <?php if($order->isShippingToIwes==null) { ?>
+            <br>
+            <p style="font-weight:bold"><?php echo $address['name'] . ' ' . $address['surname']; ?><br>
+                <?php if (isset ($address['Company'])) {
+                    echo $address['Company'] . '<br>';
                 }
-            }else{
+                ?>
+            </p>
+            <?php echo $address['address']; ?><br>
+            <?php echo $address['postcode'] . ' ' . $address['city'] . ' ' . $address['province'] ?><br>
+            <?php echo $country; ?><br>
+            <br>
+            <?php echo '</td>';
+        }else{?>
+            <br>
+            <p style="font-weight:bold">Iwes  International Web Ecommerce Services snc<br>
+            </p>
+            via Cesare Pavese , 1 <br>
+            62012 Civitanova Marche (MC)<br>
+            ITALIA<br>
+            <br>
+            </td>
+        <? }
+        echo '<td>';
+        if (in_array('0', $checkOrigin, true)) {
+            "<p style='font-weight:bold'>Prego utilizzare un nastro neutro per il confezionamento del pacco<br></p>";
+        }
+        echo '</td>';
+        echo '<td>';
+        if($line['remoteShopSellerId']!=44) {
+            $findShopParallel = \Monkey::app()->repoFactory->create('Shop')->findOneBy(['id' => $line['shopId']]);
+            if ($findShopParallel->hasEcommerce == 1) {
+                echo '<a href="' . $findShopParallel->urlSite . '/blueseal/friend/ordini" target="_blank">confermare la riga </a>';
+            } else {
                 echo '<a href="https://www.iwes.pro/blueseal/friend/ordini" target="_blank">confermare la riga </a>';
             }
-            echo '</td>';
-            echo '</tr>';
+        }else{
+            echo '<a href="https://www.iwes.pro/blueseal/friend/ordini" target="_blank">confermare la riga </a>';
         }
+        echo '</td>';
+        echo '</tr>';
+    }
 
-        ?>
+
+    ?>
 
 
-    </table>
-    <br>
-    <a href="https://www.iwes.pro/blueseal/friend/ordini" target="_blank">Affrettati a confermare</a> e riceverai notifica del pagamento del prodotto e la prenotazione del corriere per il ritiro.<br>
+</table>
+<br>
+Affrettati a confermare le richieste  e riceverai notifica del pagamento dei prodotti e la prenotazione del corriere per il ritiro.<br>
 
 
 
 <p style="font-weight:bold"><br></p>
-    <p style="font-weight:bold">In caso di domande, richieste o suggerimenti, non esitate a contattarci tramite telefono al seguente numero 0733-471365 o via e-mail all'indirizzo friends@iwes.it<br></p>
+<p style="font-weight:bold">In caso di domande, richieste o suggerimenti, non esitate a contattarci tramite telefono al seguente numero 0733-471365 o via e-mail all'indirizzo friends@iwes.it<br></p>
 
-    Saluti<br>
+Saluti<br>
 
-    <img src="https://www.pickyshop.com/it/assets/logowide.png"><br>
-    Friends Support
+<img src="https://www.pickyshop.com/it/assets/logowide.png"><br>
+Friends Support
 </body>
 </html>
