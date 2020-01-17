0c14eb3b3720fe7169708739a8843781faf6abc2
juri fiorani <jurif@hotmail.com>
2020-01-17 17:21:24 +0100

    creazione Vista Vendite

--- public/test/testJsonDecode.php
+++ public/test/testJsonDecode.php
@@ -12,7 +12,7 @@ $productRepo = \Monkey::app()->repoFactory->create('Product');
 $productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
 
 $marketplaceAccounts = $marketplaceAccountRepo->findBy(['id' => 32,'marketplaceId' => 9]);
-
+$i=0;
 foreach ($marketplaceAccounts as $marketplaceAccount) {
     $activeAutomatic = isset($marketplaceAccount->config['activeAutomatic']) ? $marketplaceAccount->config['activeAutomatic'] : 0;
 
@@ -23,18 +23,22 @@ foreach ($marketplaceAccounts as $marketplaceAccount) {
     $rows = [];
     $productBrands = [];
     $shops = [];
-
+    $prova='';
+    $countProduct=0;
     $filters = explode(',',json_encode($marketplaceAccount->config['ruleOption'],false));
     foreach ($filters as $filter) {
         $brandShop = explode('-',$filter);
         $brand = $brandShop[0];
         $shopD = $brandShop[1];
         $products = $productRepo->findBy(['productBrandId' => $brand,'productStatusId' => 6]);
+
         foreach ($products as $product) {
             if ($product->qty >= 1) {
                 $productSku = $productSkuRepo->findOneBy(['productId' => $product->id,'productVariantId' => $product->productVariantId,'shopId' => $shopD]);
                 if ($productSku != null) {
-                    $rows[] = [$productSku->productId . '-' . $product->productVariantId];
+                    array_push($rows, [$productSku -> productId . '-' . $product -> productVariantId]);
+                    $i++;
+                    $prova.=$productSku->productId.'-'.$product->productVariantId.'<br>';
                 }
 
             } else {
@@ -43,5 +47,5 @@ foreach ($marketplaceAccounts as $marketplaceAccount) {
         }
 
     }
-    var_dump($rows);
+    echo $prova;
 }
