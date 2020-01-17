c1012e159bcab91d450862377eddc94097e66851
juri fiorani <jurif@hotmail.com>
2020-01-17 13:19:17 +0100

    creazione Vista Vendite

--- public/test/testJsonDecode.php
+++ public/test/testJsonDecode.php
@@ -1,22 +1,47 @@
 <?php
+
 use bamboo\domain\entities\CMarketplaceAccount;
 
 $ttime = microtime(true);
 $time = microtime(true);
 require '../../iwesStatic.php';
 
-//$marketplaceAccountRepo = \Monkey::app()->repoFactory->create('MarketplaceAccount');
-//$marketplaceAccounts = $marketplaceAccountRepo->findOneBy(['id'=>32,'marketplaceId'=>9]);
-$marketplaceAccount = \Monkey::app()->repoFactory->create('MarketplaceAccount')->findOneBy(['id' => '32','marketplaceId' => '9']);
+$marketplaceAccountRepo = \Monkey::app()->repoFactory->create('MarketplaceAccount');
+$productBrandRepo = \Monkey::app()->repoFactory->create('ProductBrand');
+$productRepo = \Monkey::app()->repoFactory->create('Product');
+$productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
 
-$marketplaceConfig = json_decode($marketplaceAccount->config,false);
+$marketplaceAccounts = $marketplaceAccountRepo->findBy(['id' => 32,'marketplaceId' => 9]);
 
+foreach ($marketplaceAccounts as $marketplaceAccount) {
+    $activeAutomatic = isset($marketplaceAccount->config['activeAutomatic']) ? $marketplaceAccount->config['activeAutomatic'] : 0;
 
+    $isActive = isset($marketplaceAccount->config['isActive']) ? $marketplaceAccount->config['isActive'] : 0;
+    if ($isActive == 0) {
+        continue;
+    }
+    $rows = [];
+    $productBrands = [];
+    $shops = [];
 
+    $filters = explode(',',json_encode($marketplaceAccount->config['ruleOption'],false));
+    foreach ($filters as $filter) {
+        $brandShop = explode('-',$filter);
+        $brand = $brandShop[0];
+        $shopD = $brandShop[1];
+        $products = $productRepo->findBy(['productBrandId' => $brand,'productStatusId' => 6]);
+        foreach ($products as $product) {
+            if ($product->qty >= 1) {
+                $productSku = $productSkuRepo->findOneBy(['productId' => $product->id,'productVariantId' => $product->productVariantId,'shopId' => $shopD]);
+                if ($productSku != null) {
+                    $rows[] = [$productSku->productId . '-' . $product->productVariantId];
+                }
 
-    $rows=[];
-    $productBrands=[];
-    $shops=[];
+            } else {
+                continue;
+            }
+        }
 
-    $filter=json_decode($marketplaceAccount->config['ruleOption'],false);
-
+    }
+    var_dump($rows);
+}
