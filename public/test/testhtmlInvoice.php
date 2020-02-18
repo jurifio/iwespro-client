f9a3d5009c2b9828e78b26d7640dc3a04fd3ba05
juri fiorani <jurif@hotmail.com>
2020-02-18 19:45:09 +0100

    creazione Vista Vendite

---
+++ public/test/testhtmlInvoice.php
@@ -0,0 +1,426 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head><meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="icon" type="image/x-icon" sizes="32x32" href="/assets/img/favicon32.png"/>
+    <link rel="icon" type="image/x-icon" sizes="256x256" href="/assets/img/favicon256.png"/>
+    <link rel="icon" type="image/x-icon" sizes="16x16" href="/assets/img/favicon16.png"/>
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="256x256" href="/assets/img/favicon256.png"/>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
+    <script>
+        paceOptions = {
+            ajax: {ignoreURLs: ['/blueseal/xhr/TemplateFetchController', '/blueseal/xhr/CheckPermission']}
+        }
+    </script>
+    <link type="text/css" href="https://www.iwes.pro/assets/css/pace.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen,print"/>
+    <link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" media="screen,print"/>
+    <link type="text/css" href="https://code.jquery.com/ui/1.11.4/themes/flick/jquery-ui.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://s3-eu-west-1.amazonaws.com/bamboo-css/jquery.scrollbar.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://s3-eu-west-1.amazonaws.com/bamboo-css/bootstrap-colorpicker.min.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://github.com/mar10/fancytree/blob/master/dist/skin-common.less" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.24.0/skin-bootstrap/ui.fancytree.min.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.min.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/basic.min.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://www.iwes.pro/assets/css/ui.dynatree.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.6.16/summernote.min.css" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300" rel="stylesheet" media="screen"/>
+    <link type="text/css" href="https://raw.githubusercontent.com/kleinejan/titatoggle/master/dist/titatoggle-dist-min.css" rel="stylesheet" media="screen,print"/>
+    <link type="text/css" href="https://www.iwes.pro/assets/css/pages-icons.css" rel="stylesheet" media="screen,print"/>
+    <link type="text/css" href="https://www.iwes.pro/assets/css/pages.css" rel="stylesheet" media="screen,print"/>
+    <link type="text/css" href="https://www.iwes.pro/assets/css/style.css" rel="stylesheet" media="screen,print"/>
+    <link type="text/css" href="https://www.iwes.pro/assets/css/fullcalendar.css" rel="stylesheet" media="screen,print"/>
+    <script  type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
+    <script  type="application/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script  type="application/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+    <script  type="application/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script defer type="application/javascript" src="https://www.iwes.pro/assets/js/pages.js"></script>
+    <script defer type="application/javascript" src="https://www.iwes.pro/assets/js/blueseal.prototype.js"></script>
+    <script defer type="application/javascript" src="https://www.iwes.pro/assets/js/blueseal.ui.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script defer type="application/javascript" src="https://cdn.jsdelivr.net/jquery.bez/1.0.11/jquery.bez.min.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/unveil/1.3.0/jquery.unveil.min.js"></script>
+    <script defer type="application/javascript" src="https://s3-eu-west-1.amazonaws.com/bamboo-js/jquery.scrollbar.min.js"></script>
+    <script defer type="application/javascript" src="https://www.iwes.pro/assets/js/Sortable.min.js"></script>
+    <script defer type="application/javascript" src="https://s3-eu-west-1.amazonaws.com/bamboo-js/bootstrap-colorpicker.min.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.24.0/jquery.fancytree-all.min.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone-amd-module.min.js"></script>
+    <script defer type="application/javascript" src="https://cdn.jsdelivr.net/jquery.dynatree/1.2.4/jquery.dynatree.min.js"></script>
+    <script defer type="application/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.6.16/summernote.min.js"></script>
+    <script defer type="application/javascript" src="https://s3-eu-west-1.amazonaws.com/bamboo-js/summernote-it-IT.js"></script>
+    <script defer type="application/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
+    <script defer type="application/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/additional-methods.min.js"></script>
+    <script defer type="application/javascript" src="https://www.iwes.pro/assets/js/blueseal.kickstart.js"></script>
+    <script  type="application/javascript" src="https://www.iwes.pro/assets/js/monkeyUtil.js"></script>
+    <script defer type="application/javascript" src="https://www.iwes.pro/assets/js/invoice_print.js"></script>
+    <script defer async type="application/javascript" src="https://www.iwes.pro/assets/js/blueseal.common.js"></script>
+    <title>BlueSeal - Stampa fattura</title>
+    <style type="text/css">
+        @page {
+            size: A4;
+            margin: 5mm 0mm 0mm 0mm;
+        }
+
+        @media print {
+            body {
+                zoom: 100%;
+                width: 800px;
+                height: 1100px;
+                overflow: hidden;
+            }
+
+            .container {
+                width: 100%;
+            }
+
+            .newpage {
+                page-break-before: always;
+                page-break-after: always;
+                page-break-inside: avoid;
+            }
+
+            @page {
+                size: A4;
+                margin: 5mm 0mm 0mm 0mm;
+            }
+
+            .cover {
+                display: none;
+            }
+
+            .page-container {
+                display: block;
+            }
+
+            /*remove chrome links*/
+            a[href]:after {
+                content: none !important;
+            }
+
+            .col-md-1,
+            .col-md-2,
+            .col-md-3,
+            .col-md-4,
+            .col-md-5,
+            .col-md-6,
+            .col-md-7,
+            .col-md-8,
+            .col-md-9,
+            .col-md-10,
+            .col-md-11,
+            .col-md-12 {
+                float: left;
+            }
+
+            .col-md-12 {
+                width: 100%;
+            }
+
+            .col-md-11 {
+                width: 91.66666666666666%;
+            }
+
+            .col-md-10 {
+                width: 83.33333333333334%;
+            }
+
+            .col-md-9 {
+                width: 75%;
+            }
+
+            .col-md-8 {
+                width: 66.66666666666666%;
+            }
+
+            .col-md-7 {
+                width: 58.333333333333336%;
+            }
+
+            .col-md-6 {
+                width: 50%;
+            }
+
+            .col-md-5 {
+                width: 41.66666666666667%;
+            }
+
+            .col-md-4 {
+                width: 33.33333333333333%;
+            }
+
+            .col-md-3 {
+                width: 25%;
+            }
+
+            .col-md-2 {
+                width: 16.666666666666664%;
+            }
+
+            .col-md-1 {
+                width: 8.333333333333332%;
+            }
+
+            .col-md-pull-12 {
+                right: 100%;
+            }
+
+            .col-md-pull-11 {
+                right: 91.66666666666666%;
+            }
+
+            .col-md-pull-10 {
+                right: 83.33333333333334%;
+            }
+
+            .col-md-pull-9 {
+                right: 75%;
+            }
+
+            .col-md-pull-8 {
+                right: 66.66666666666666%;
+            }
+
+            .col-md-pull-7 {
+                right: 58.333333333333336%;
+            }
+
+            .col-md-pull-6 {
+                right: 50%;
+            }
+
+            .col-md-pull-5 {
+                right: 41.66666666666667%;
+            }
+
+            .col-md-pull-4 {
+                right: 33.33333333333333%;
+            }
+
+            .col-md-pull-3 {
+                right: 25%;
+            }
+
+            .col-md-pull-2 {
+                right: 16.666666666666664%;
+            }
+
+            .col-md-pull-1 {
+                right: 8.333333333333332%;
+            }
+
+            .col-md-pull-0 {
+                right: 0;
+            }
+
+            .col-md-push-12 {
+                left: 100%;
+            }
+
+            .col-md-push-11 {
+                left: 91.66666666666666%;
+            }
+
+            .col-md-push-10 {
+                left: 83.33333333333334%;
+            }
+
+            .col-md-push-9 {
+                left: 75%;
+            }
+
+            .col-md-push-8 {
+                left: 66.66666666666666%;
+            }
+
+            .col-md-push-7 {
+                left: 58.333333333333336%;
+            }
+
+            .col-md-push-6 {
+                left: 50%;
+            }
+
+            .col-md-push-5 {
+                left: 41.66666666666667%;
+            }
+
+            .col-md-push-4 {
+                left: 33.33333333333333%;
+            }
+
+            .col-md-push-3 {
+                left: 25%;
+            }
+
+            .col-md-push-2 {
+                left: 16.666666666666664%;
+            }
+
+            .col-md-push-1 {
+                left: 8.333333333333332%;
+            }
+
+            .col-md-push-0 {
+                left: 0;
+            }
+
+            .col-md-offset-12 {
+                margin-left: 100%;
+            }
+
+            .col-md-offset-11 {
+                margin-left: 91.66666666666666%;
+            }
+
+            .col-md-offset-10 {
+                margin-left: 83.33333333333334%;
+            }
+
+            .col-md-offset-9 {
+                margin-left: 75%;
+            }
+
+            .col-md-offset-8 {
+                margin-left: 66.66666666666666%;
+            }
+
+            .col-md-offset-7 {
+                margin-left: 58.333333333333336%;
+            }
+
+            .col-md-offset-6 {
+                margin-left: 50%;
+            }
+
+            .col-md-offset-5 {
+                margin-left: 41.66666666666667%;
+            }
+
+            .col-md-offset-4 {
+                margin-left: 33.33333333333333%;
+            }
+
+            .col-md-offset-3 {
+                margin-left: 25%;
+            }
+
+            .col-md-offset-2 {
+                margin-left: 16.666666666666664%;
+            }
+
+            .col-md-offset-1 {
+                margin-left: 8.333333333333332%;
+            }
+
+            .col-md-offset-0 {
+                margin-left: 0;
+            }
+        }
+    </style>
+</head>
+<body class="fixed-header">
+
+<!--start-->
+<div class="container container-fixed-lg">
+
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="invoice padding-50 sm-padding-10">
+                <div>
+                    <div class="pull-left">
+                        <!--logo negozio-->
+                        <img width="235" height="47" alt="" class="invoice-logo"
+                             data-src-retina="/assets/img/LogoIwes.png" data-src="/assets/img/LogoIwes.png" src="/assets/img/LogoIwes.png">
+                        <!--indirizzo negozio-->
+                        <br><br>
+                        <address class="m-t-10"><b>Iwes Snc
+                                <br>International Web Ecommerce Services</b>
+                            <br>Via Cesare Pavese, 1
+                            <br>62012 - Civitanova Marche (MC)
+                            <br>P.IVA: 01865380438
+                            <br>Tel.: +39.0733.471365
+                            <br>email:support@pickyshop.com
+                        </address>
+                        <br>
+                        <div>
+                            <div class="pull-left font-montserrat all-caps small">
+                                <strong>Fattura</strong>  19/W<strong> del </strong>18-02-2020
+                            </div>
+                        </div>
+                        <div><br>
+                            <div class="pull-left font-montserrat small"><strong>Rif. Contratto N. </strong>  1 del 05-02-2020</div>
+                        </div>
+                        <div><br>
+                            <div class="pull-left font-montserrat small"><strong></strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pull-right sm-m-t-0">
+                        <h2 class="font-montserrat all-caps hint-text">Fattura</h2>
+
+                        <div class="col-md-12 col-sm-height sm-padding-20">
+                            <p class="small no-margin">Intestata a</p><h5 class="semi-bold m-t-0 no-margin">barbagallo</h5><address><strong>via catania 0<br>023145 catanaia (ct)<br>Italy<br>C.FISC. o P.IVA: 12345678900</strong></address><div class="clearfix"></div><br><p class="small no-margin"></p><address><strong><br><br><br></address></div></div></div></div><table class="table invoice-table m-t-0"><thead>
+                <!--tabella prodotti-->
+                <tr><th class="small">Descrizione Prodotto</th><th class="text-center small">Importo</th><th class="text-center small">iva<th class="text-center small">Importo Totale</th></th></tr></thead><tbody><tr><td class="text-center">Servizio MonkSource</td><td class="text-center">600.00 &euro;</td><td class="text-center">22%: 132.00 &euro;</td><td class="text-center">732.00 &euro;</td></tr><tr><td class="text-center">monksource-monkSource<br>monksource servizi hosting<br></td><td class="text-center">600.00 &euro;</td><td class="text-center">22%: 132.00 &euro;</td><td class="text-center">732.00 &euro;</td></tr><tr><td class="text-center">monksource-monkSource<br>monksource servizi hosting<br></td><td class="text-center">600.00 &euro;</td><td class="text-center">22%: 132.00 &euro;</td><td class="text-center">732.00 &euro;</td></tr><tr><td class="text-center">monksource-monkSource<br>monksource servizi hosting<br></td><td class="text-center">600.00 &euro;</td><td class="text-center">22%: 132.00 &euro;</td><td class="text-center">732.00 &euro;</td></tr><tr><td class="text-center">monksource-monkSource<br>monksource servizi hosting<br></td><td class="text-center">600.00 &euro;</td><td class="text-center">22%: 132.00 &euro;</td><td class="text-center">732.00 &euro;</td></tr></tbody><br><tr class="text-left font-montserrat small">
+                    <td style="border: 0px"></td>
+                    <td style="border: 0px"></td>
+                    <td style="border: 0px">
+                        <strong>Totale Netto</strong></td>
+                    <td style="border: 0px"
+                        class="text-center">3000.00 &euro;</td>
+                </tr><tr style="border: 0px" class="text-left font-montserrat small hint-text">
+                    <td style="border: 0px"></td>
+                    <td style="border: 0px"></td>
+                    <td style="border: 0px">
+                        <strong>Totale Tasse</strong></td><td style="border: 0px" class="text-center">660.00 &euro;</td></tr><tr style="border: 0px" class="text-left font-montserrat small hint-text">
+                    <td style="border: 0px"></td>
+                    <td style="border: 0px"></td>
+                    <td style="border: 0px">
+                        <strong>Totale Dovuto</strong></td><td style="border: 0px" class="text-center">3660.00 &euro;</td></tr><tr style="border: 0px" class="text-center">
+                    <td colspan="2" style="border: 0px">Modalità di pagamento Bonifico 30/60gg d.f.</td> <td style="border: 0px"></td> <td style="border: 0px"></td></tr><tr style="border: 0px" class="text-center">
+                    <td colspan="2" style="border: 0px">1830.00  &euro; da corrisponedere entro il 19-03-2020</td> <td style="border: 0px"></td> <td style="border: 0px"></td></tr><tr style="border: 0px" class="text-center">
+                    <td colspan="2" style="border: 0px">1830.00  &euro; da corrisponedere entro il 18-05-2020</td> <td style="border: 0px"></td> <td style="border: 0px"></td></tr></table>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div>
+            <center><img alt="" class="invoice-thank" data-src-retina="/assets/img//assets/img/invoicethankyou.jpg"
+                         data-src="/assets/img//assets/img/invoicethankyou.jpg" src="/assets/img//assets/img/invoicethankyou.jpg">
+            </center>
+        </div>
+        <br>
+        <br>
+    </div>
+</div>
+</div><!--end--><script type="application/javascript">
+    $(document).ready(function () {
+
+        Pace.on('done', function () {
+
+            setTimeout(function () {
+                window.print();
+
+                setTimeout(function () {
+                    window.close();
+                }, 1);
+
+            }, 200);
+
+        });
+    });
+</script>
+</body>
+</html>
