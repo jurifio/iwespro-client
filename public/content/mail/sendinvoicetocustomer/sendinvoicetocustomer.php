8e1966bd73ec59d30905aab8179b29b305a15d4e
juri fiorani <jurif@hotmail.com>
2020-03-20 09:42:16 +0100

    creazione Vista Vendite

---
+++ public/content/mail/sendinvoicetocustomer/sendinvoicetocustomer.php
@@ -0,0 +1,235 @@
+<?php
+use bamboo\core\theming\CMailerHelper;
+/** @var CMailerHelper $app */ ?>
+<html lang="<?php echo $app->lang(); ?>">
+<head>
+    <meta charset="UTF-8"/>
+    <style type="text/css"><?php echo $css; ?></style>
+</head>
+<body style="margin: 0px">
+<table align="center" width="100%" cellpadding="0" cellspacing="0" border="0" data-mobile="true" dir="ltr"
+       data-width="600" style="font-size: 14px; background-color:#f3f3f3; font-family:Helvetica,Arial,sans-serif">
+    <tbody>
+    <tr>
+        <td align="center" valign="top" style="margin:0;padding:42px 0;">
+            <table align="center" border="0" cellspacing="0" cellpadding="0" width="600" bgcolor="white"
+                   style="background-position: center top; background-repeat: no-repeat; background-size: cover; width: 660px;"
+                   class="wrapper">
+                <tbody>
+                <tr>
+                    <td align="center" valign="top" style="margin:0;padding:0;">
+                        <table border="0" cellpadding="0" cellspacing="0" align="center" data-editable="image"
+                               data-mobile-width="0" width="140">
+                            <tbody>
+                            <tr>
+                                <td valign="top" align="center"
+                                    style="display: inline-block; padding: 20px 0px 10px; margin: 0px;" class="tdBlock">
+                                    <a href="https:www.pickyshop.com" target="_blank">
+                                        <img src="https://cdn.iwes.it/assets/logoIwes.png" alt="" height="80"
+                                             border="0"
+                                             style="border-width: 0px; border-style: none; border-color: transparent; font-size: 12px; display: block;"/>
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" align="center" style="padding:10px 10px 0px 10px; margin:0;">
+                        <table align="center" width="620px" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0"
+                               data-editable="text">
+                            <tbody>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 30px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                        Inviamo di seguito la fattura proforma n. <?php echo $numberInvoice ?> per  € <?php echo number_format($slipTotalAmount,2,',','.') ?>
+                                   con Scadenza <?php echo $invoiceDate;?>.
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                In  allegato Troverà la fattura e tutto il relativo dettaglio.
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                Di Seguito comunichiamo i nostri dati bancari e l'elenco delle scadenze generate per la fattura allegata
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 20px 10px 0; margin: 0px; line-height: 1.5; font-size: 18px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:800;color:#3A3A3A; line-height:1.2;">
+                                    DATI BANCARI
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                    Beneficiary: <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2; font-weight: 600">International Web Ecommerce Sevices snc</span>
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                        Iban: <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2; font-weight: 600">IT 48L 05216 1340 0000 00000 2334</span>
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                    Bank: CREDITO VALTELLINESE
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 10px; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                    BIC/SWIFT: BPCVT2S
+                                    </span>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" align="center" style="padding:10px 10px 0px 10px; margin:0;">
+                        <table align="center" width="620px" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0"
+                               data-editable="text">
+                            <thead>
+                            <tr>
+                                <th valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:800;color:#3A3A3A; line-height:1.2;">
+                                tipo Scadenza
+                                </span>
+                                </th>
+                                <th valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:800;color:#3A3A3A; line-height:1.2;">
+                                data Scadenza
+                                        </span>
+                                </th>
+                                <th valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:800;color:#3A3A3A; line-height:1.2;">
+                               Importo Scadenza
+                                        </span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                          <?php  foreach ($btt as $payment){
+                                $brp=\Monkey::app()->repoFactory->create('BillRegistryTypePayment')->findOneBy(['id'=>$payment->billRegistryTypePaymentId]);
+                                ?>
+                                <tr>
+                                    <td valign="top" align="left" class="lh-3"
+                                        style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                        <?php echo $brp->name; ?>
+                                    </span>
+                                    </td>
+                                    <td valign="top" align="left" class="lh-3"
+                                        style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                        <?php echo \bamboo\utils\time\STimeToolbox::EurFormattedDate($payment->dateEstimated); ?>
+                                    </span>
+                                    </td>
+
+                                    <td valign="top" align="left" class="lh-3"
+                                        style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                         <?php
+                                         echo number_format($payment->amountPayment,2,',','.'); ?>
+                                    </span>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 10px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:bold;color:#3A3A3A; line-height:1.2;">
+  <?php
+
+  echo number_format($amountTotal,2,',','.'); ?> &euro;
+                                    </span>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" align="center" style="padding:10px 10px 0px 10px; margin:0;">
+                        <table align="center" width="620px" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0"
+                               data-editable="text">
+                            <tbody>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 20px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                        per qualsiasi informazione  potete  telefonare al numero 0733 471365 o scrivere a  : <a style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#909090; line-height:1.2" href="mailto:billing@iwes.it">billing@iwes.it</a>.
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 20px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                        Cordialmente salutiamo.
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 20px 10px 0; margin: 0px; line-height: 1.5; font-size: 18px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:800;color:#3A3A3A; line-height:1.2;">
+                                     I.W.E.S.
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 0px 10px 0; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#878787; line-height:1.2;">
+                                        International Web E-commerce Services
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top" align="left" class="lh-3"
+                                    style="padding: 2px 10px 20px; margin: 0px; line-height: 1.5; font-size: 16px; font-family: Times New Roman, Times, serif;">
+                                    <span style="font-family: 'Poppins', sans-serif; font-size:15px;font-weight:300;color:#3A3A3A; line-height:1.2;">
+                                     Financial Department
+                                    </span>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
