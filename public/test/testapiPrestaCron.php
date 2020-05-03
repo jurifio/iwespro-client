6ad3c1df51da5aa268fa3ead92e1d89019625682
juri fiorani <jurif@hotmail.com>
2020-05-03 19:00:08 +0200

    creazione Vista Vendite

--- public/test/testapiPrestaCron.php
+++ public/test/testapiPrestaCron.php
@@ -32,6 +32,12 @@ $time = microtime(true);
 $monkey->eventManager;
 var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
 $time = microtime(true);
+$now=((new \DateTime())->format(DateTime::ATOM));
+$createTimeFrom=new \DateTime();
+$createTimeFrom->modify('- 1 hour');
+$create=$createTimeFrom->format(DateTime::ATOM);
+echo '<br>'.$create.'<br>';
+echo $now;
 
 $today=((new \DateTime())->format('Y-m-d H:i:s'));
 echo $today;
@@ -45,8 +51,8 @@ $request='<?xml version="1.0" encoding="utf-8"?>
     <eBayAuthToken>AgAAAA**AQAAAA**aAAAAA**11TMXA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6AFlIekDZKGpw2dj6x9nY+seQ**2noBAA**AAMAAA**e4WFyEzST7194ENmESCHB2uW8jxU5xxolrf7Nzy/8LOnKXWp5+PXmWQ6D7kl6eETCU9wshzpVtNTheq7+Wj99W19oACUqe2fTQgzWPiS1o0UlUgUvcdx8nvsVRBbZN/fuSSd12v+bZ9P7gukf09DSHq6wXuhmLxkx8nlrBmHXUBLgYT4e9tMkrGwASQq3uEdoHuUlihbwQxsOqLNv1U9yz0OO30FY7xvOvHR137Lz5CVW9q2IdjSsY45MmAMU8UbMsebDVruoKU59rRTk1kRqrzypuZXB8oo0qq2HNgPzHDBzr70S63ZVfg5QOMunvAIQOfTRGTjy4Jcl/YnIOsZclQRQbPLngZemCouDL+HoybdDKxtq/sSAVBV3EurGYGBYgsRVPv7WBmaPW1LJ5IfQhOosV3Tq9b7CDWupzzxFO9S5lpg4VGts+lsy04dEo5eMUgjWKeofdtMdkOSu2WaEDrFpR4L+kVYcZytqmDFipHZ4crpFhW++gzQthXtOi5wYyhjvEzf6Y82aD9tS9io/TYAiHhzUZhNOv3ofZQRyuJ5rkNwTqbaiqJuQJ7ZLtwnKO+o6cYi7QyJ928t8UEgmzoxvaA5pX1qmUbryz9b8lnPpUeVImOcxPKms8QS6/yL+Hj9iSXBADvcSGvU0lM4Rj6KRTPRsehSCIcWd2pQZ3SE++7ql13AHEyiwlHzvM+h+XM6yEpo40yo7EcwjNSWxqcsh6J+Hg4IJuFDlS5zW21mnz/0mBIaX7r8efcUnR+v</eBayAuthToken>
   </RequesterCredentials>
   <WarningLevel>High</WarningLevel>
-  <CreateTimeFrom>2019-10-01T20:34:44.000Z</CreateTimeFrom>
-  <CreateTimeTo>2020-10-30T20:34:44.000Z</CreateTimeTo>
+  <CreateTimeFrom>2019-10-01T20:34:44</CreateTimeFrom>
+  <CreateTimeTo>'.$create.'</CreateTimeTo>
   <OrderRole>Seller</OrderRole>
 </GetOrdersRequest>';
 
