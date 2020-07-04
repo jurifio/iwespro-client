1932e6c198a45e664d488235aa1b279e4fce13a5
juri fiorani <jurif@hotmail.com>
2020-07-04 12:41:37 +0200

    creazione Vista Vendite

---
+++ public/test/testarraycheck.php
@@ -0,0 +1,42 @@
+<?php
+ini_set("memory_limit", "2000M");
+ini_set('max_execution_time', 0);
+$ttime = microtime(true);
+$time = microtime(true);
+require '../../iwesStatic.php';
+var_dump('Applicazione  \t\t\t\t' . (microtime(true) - $time));
+
+$monkey = \Monkey::app();
+$time = microtime(true);
+$monkey->dbAdapter;
+var_dump("dbAdapter \t\t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->cacheService;
+var_dump("cacheService \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->sessionManager;
+var_dump("sessionManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->authManager;
+var_dump("authManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->entityManagerFactory;
+var_dump("entityManagerFactory \t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->repoFactory;
+var_dump("repoFactory \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->eventManager;
+var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+echo  number_format(100 * (100 - 80) /100, 2);
+$dirtySku = [];
+$k = 0;
+array_push($dirtySku,['extSizeId'=>15,'size'=>38]);
+array_push($dirtySku,['extSizeId'=>16,'size'=>39]);
+foreach($dirtySku as $key=>$value){
+    if($value['extSizeId']==15){
+       echo $value['size'];
+        break;
+    }
+}
