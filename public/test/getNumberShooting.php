faf1b2b6813acba68dc39f76db7645f10a5632fc
juri fiorani <jurif@hotmail.com>
2020-09-10 19:37:45 +0200

    creazione Vista Vendite

--- public/test/getNumberShooting.php
+++ public/test/getNumberShooting.php
@@ -5,51 +5,51 @@ $shootingRepo=\Monkey::app()->repoFactory->create('Shooting');
 $shootingDocumentRepo=\Monkey::app()->repoFactory->create('Document');
 $userRepo=\Monkey::app()->repoFactory->create('User');
 
-if($_GET['shopId']){
-    $shopId=$_GET['shopId'];
+if($_POST['shopId']){
+    $shopId=$_POST['shopId'];
 }
-if($_GET['Calzature']){
-    $Calzature=$_GET['Calzature'];
+if($_POST['Calzature']){
+    $Calzature=$_POST['Calzature'];
 }else{
     $Calzature=0;
 }
-if($_GET['Borse']){
-    $Borse=$_GET['Borse'];
+if($_POST['Borse']){
+    $Borse=$_POST['Borse'];
 }else{
     $Borse=0;
 }
-if($_GET['Accessori']){
-    $Accessori=$_GET['Accessori'];
+if($_POST['Accessori']){
+    $Accessori=$_POST['Accessori'];
 }else{
     $Accessori=0;
 }
-if($_GET['AbbD']){
-    $AbbD=$_GET['AbbD'];
+if($_POST['AbbD']){
+    $AbbD=$_POST['AbbD'];
 }else{
     $AbbD=0;
 }
-if($_GET['AbbU']){
-    $AbbU=$_GET['AbbU'];
+if($_POST['AbbU']){
+    $AbbU=$_POST['AbbU'];
 }else{
     $AbbU=0;
 }
-if($_GET['AbbB2']){
-    $AbbB2=$_GET['AbbB2'];
+if($_POST['AbbB2']){
+    $AbbB2=$_POST['AbbB2'];
 }else{
     $AbbB2=0;
 }
-if($_GET['AbbB']){
-    $AbbB=$_GET['AbbB'];
+if($_POST['AbbB']){
+    $AbbB=$_POST['AbbB'];
 }else{
     $AbbB=0;
 }
-if($_GET['CurvD']){
-    $CurvD=$_GET['CurvD'];
+if($_POST['CurvD']){
+    $CurvD=$_POST['CurvD'];
 }else{
     $CurvD=0;
 }
-if($_GET['CurvU']){
-    $CurvU=$_GET['CurvU'];
+if($_POST['CurvU']){
+    $CurvU=$_POST['CurvU'];
 }else{
     $CurvU=0;
 }
