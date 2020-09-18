b85d74a95baf52610149049973db33fe60d8e518
juri fiorani <jurif@hotmail.com>
2020-09-18 16:33:52 +0200

    creazione Vista Vendite

---
+++ public/test/postProductShooting.php
@@ -0,0 +1,29 @@
+<?php
+
+use bamboo\domain\repositories\CProductHasShootingRepo;
+
+require '../../iwesStatic.php';
+
+if($_POST['productId']){
+    $productId=$_POST['productId'];
+}
+if($_POST['productVariantId']){
+    $productVariantId=$_POST['productVariantId'];
+}
+if($_POST['shootingId']){
+    $shootingId=$_POST['shootingId'];
+}
+$productsInformation[]=[$productId.'-'.$productVariantId,'noSize'.'-'.$productId.'-'.$productVariantId,'noQty'.'-'.$productId.'-'.$productVariantId];
+
+$data='1';
+$i=0;
+$productsIds[]=[$productId.'-'.$productVariantId];
+/** @var CProductHasShootingRepo $pHsRepo */
+$pHsRepo = \Monkey::app()->repoFactory->create('ProductHasShooting');
+
+$association = $pHsRepo->associateNewProductsToShooting($productsIds, $shooting->id, $productsInformation);
+
+
+echo json_encode($data);
+
+
