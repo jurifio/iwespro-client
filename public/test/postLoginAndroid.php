c65a242e32846495a00ff2794f402641d2d83550
juri fiorani <jurif@hotmail.com>
2020-09-19 16:24:04 +0200

    creazione Vista Vendite

--- public/test/postLoginAndroid.php
+++ public/test/postLoginAndroid.php
@@ -6,13 +6,25 @@ require '../../iwesStatic.php';
 if($_POST['email']){
     $email=$_POST['email'];
 }
-$data='';
+$data=[];
 $user=\Monkey::app()->repoFactory->create('User')->findOneBy(['email'=>$email]);
 if($user!=null){
     $userDetails=\Monkey::app()->repoFactory->create('UserDetails')->findOneBy(['userId'=>$user->id]);
-    $data='Ciao '. $userDetails->name;
+    $resShop=\Monkey::app()->dbAdapter->query('select shopId as shopId from UserHasShop where userId='.$user->id,[])->fetchAll();
+    foreach($resShop as $shopResult) {
+        $shopId=$shopResult['shopId'];
+    }
+    $resEditorialPlan=\Monkey::app()->dbAdapter->query('select id as editorialPlanId from EditorialPlan where shopId='.$shopId,[])->fetchAll();
+    if(count($resEditorialPlan)>0) {
+        foreach ($resEditorialPlan as $resEditorial) {
+            $editorialPlanId = $resEditorial['editorialPlanId'];
+        }
+    }else{
+        $editorialPlanId ='no';
+    }
+    $data[]=['name'=>'Ciao '. $userDetails->name,'shopId'=>$shopId,'editorialPlanId'=>$editorialPlanId];
 }else{
-    $data='Attenzione Email utente non Riconosciuta';
+    $data=['name'=>'Attenzione Email utente non Riconosciuta','shopId'=>'no','editorialPlanId'=>'no'];
 }
 
 
