8e9481aa3584acbbe5c90e5300041b2c93fb2a7e
juri fiorani <jurif@hotmail.com>
2020-09-21 19:54:47 +0200

    creazione Vista Vendite

--- public/test/postProductSocial.php
+++ public/test/postProductSocial.php
@@ -133,7 +133,7 @@ $title='Richiesta post  per  ' . $editorialPlanName . ' da app su scatto Social 
             $emailRepo->newMail('Iwes IT Department <it@iwes.it>',$to,$userEditor,[],$subject,$message,null,null,null,'mailGun',false,null);
         }
     }
-    $toBoss[]=['gianluca@iwes.it'];
+    $toBoss=['gianluca@iwes.it'];
     /** @var \bamboo\domain\repositories\CEmailRepo $emailRepo */
     $emailRepo->newMail('Iwes IT Department <it@iwes.it>',$toBoss,[],[],$subject,$message,null,null,null,'mailGun',false,null);
 }else{
