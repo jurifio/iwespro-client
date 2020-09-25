1585018ea8bdd6c1c57c270664899cbb652c2af3
juri fiorani <jurif@hotmail.com>
2020-09-25 16:08:48 +0200

    creazione Vista Vendite

--- public/test/getProductStockList.php
+++ public/test/getProductStockList.php
@@ -57,7 +57,7 @@ FROM `Product` `p`
       ProductHasShooting phs
       JOIN Shooting shoot ON phs.shootingId = shoot.id
       LEFT JOIN Document doc ON shoot.friendDdt = doc.id)
-    ON p.productVariantId = phs.productVariantId AND p.id = phs.productId where 1=1 and s.id=" . $shopId . "  " . $sqlEan . "  GROUP BY p.id, p.productVariantId, p.externalId
+    ON p.productVariantId = phs.productVariantId AND p.id = phs.productId where 1=1    " . $sqlEan . "  GROUP BY p.id, p.productVariantId, p.externalId
     ORDER BY `p`.`creationDate` DESC ";
 $data = [];
 $i = 0;
