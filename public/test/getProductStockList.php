0536fd176ac883d7e6dc50b2a969125bfc1a4fb3
juri fiorani <jurif@hotmail.com>
2020-09-30 11:38:07 +0200

    creazione Vista Vendite

--- public/test/getProductStockList.php
+++ public/test/getProductStockList.php
@@ -8,7 +8,7 @@ $userRepo = \Monkey::app()->repoFactory->create('User');
 $productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
 $productSizeRepo = \Monkey::app()->repoFactory->create('ProductSize');
 $productRepo = \Monkey::app()->repoFactory->create('Product');
-$productBrandRepo=\Monkey::app()->repoFactory->create('ProductBrand');
+$productBrandRepo = \Monkey::app()->repoFactory->create('ProductBrand');
 if ($_GET['email']) {
     $email = $_GET['email'];
 }
@@ -19,13 +19,17 @@ $resShop = \Monkey::app()->dbAdapter->query('select shopId as shopId from UserHa
 foreach ($resShop as $shopResult) {
     $shopId = $shopResult['shopId'];
 }
-if ($shopId==1) {
+if ($shopId == 1) {
     if ($_GET['ean'] != "0") {
         $sqlEan = " and ds.barcode= '" . substr($_GET['ean'],0,-1) . "'";
     }
-}else{
+} elseif ($shopId == 58) {
     if ($_GET['ean'] != "0") {
-        $sqlEan = " and ds.barcode= '" . $_GET['ean']. "'";
+        $sqlEan = " and ds.barcode= '" . substr($_GET['ean'],0,8) . "'";
+    }
+} else {
+    if ($_GET['ean'] != "0") {
+        $sqlEan = " and ds.barcode= '" . $_GET['ean'] . "'";
     }
 
 }
@@ -63,33 +67,29 @@ FROM `Product` `p`
       ProductHasShooting phs
       JOIN Shooting shoot ON phs.shootingId = shoot.id
       LEFT JOIN Document doc ON shoot.friendDdt = doc.id)
-    ON p.productVariantId = phs.productVariantId AND p.id = phs.productId where 1=1 and s.id=".$shopId . $sqlEan . "  GROUP BY p.id, p.productVariantId, p.externalId
+    ON p.productVariantId = phs.productVariantId AND p.id = phs.productId where 1=1 and s.id=" . $shopId . $sqlEan . "  GROUP BY p.id, p.productVariantId, p.externalId
     ORDER BY `p`.`creationDate` DESC  ";
 $data = [];
 $i = 0;
 $resultProduct = \Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
 foreach ($resultProduct as $res) {
     $product = $productRepo->findOneBy(['id' => $res['id'],'productVariantId' => $res['productVariantId']]);
-    $imagePhoto = 'https://cdn.iwes.it/'.$product->productBrand->slug.'/'.$res['id'].'-'.$res['productVariantId'].'-001-281.jpg';
+    $imagePhoto = 'https://cdn.iwes.it/' . $product->productBrand->slug . '/' . $res['id'] . '-' . $res['productVariantId'] . '-001-281.jpg';
     $ch = curl_init($imagePhoto);
-    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_setopt($ch,CURLOPT_NOBODY,true);
     curl_exec($ch);
-    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
 
     if ($code == 200) {
-        $imagePhoto = 'https://cdn.iwes.it/'.$product->productBrand->slug.'/'.$res['id'].'-'.$res['productVariantId'].'-001-281.jpg';
+        $imagePhoto = 'https://cdn.iwes.it/' . $product->productBrand->slug . '/' . $res['id'] . '-' . $res['productVariantId'] . '-001-281.jpg';
     } else {
         $imagePhoto = 'https://cdn.iwes.it/dummy/bs-dummy-16-9-281.png';
     }
     curl_close($ch);
 
-    $data[$i] = ['productId' => $res['id'],'productVariantId' => $res['productVariantId'], 'extId'=>$res['extId'],'cpf' => $res['cpf'],'brand' => $res['brand'],'season' => $res['season'],'imagePhoto' => $imagePhoto];
+    $data[$i] = ['productId' => $res['id'],'productVariantId' => $res['productVariantId'],'extId' => $res['extId'],'cpf' => $res['cpf'],'brand' => $res['brand'],'season' => $res['season'],'imagePhoto' => $imagePhoto];
     $i++;
 }
-
-
-
-
 
 
 echo json_encode($data);
