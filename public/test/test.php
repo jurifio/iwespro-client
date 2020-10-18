20d49f51bbb15bb6c583ffb194ab68999b8810eb
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2020-10-18 20:29:53 +0200

    initial commit

--- public/test/test.php
+++ public/test/test.php
@@ -29,6 +29,7 @@ $time = microtime(true);
 $monkey->eventManager;
 var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
 $time = microtime(true);
-echo  number_format(100 * (100 - 80) /100, 2);
+
+
 
 
