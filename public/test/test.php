11d9d8dc31261441321fee8a5142745774ae4a55
juri fiorani <jurif@hotmail.com>
2020-10-23 18:18:31 +0200

    creazione Vista Vendite

--- public/test/test.php
+++ public/test/test.php
@@ -31,18 +31,26 @@ var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
 $time = microtime(true);
 if (ENV == 'dev') {
     $ftp_server = 'ftp.iwes.pro';
-
-    $path = 'public_html/imgTransfer/';
-
+    $pathlocal = '/media/sf_sites/iwespro/temp-remaster/';
+    $save_to = '/home/iwespro/public_html/imgTransfer';
+    $save_to_dir = '/media/sf_sites/iwespro/temp-remaster';
+    $path = '/public_html/imgTransfer/';
+    $remotepathTodo = 'shootImport/newage2/topublish_dev/';
+    $remotepathOriginal = '/shootImport/newage2/original_dev/';
+    $remotepathToRename = '/shootImport/newage2/torename_dev/';
 
 } else {
     $ftp_server = 'ftp.iwes.pro';
-
-    $path = './';
-
+    $pathlocal = '/home/iwespro/public_html/temp-remaster/';
+    $save_to = '/home/iwespro/public_html/temp-remaster/';
+    $save_to_dir = '/home/iwespro/public_html/temp-remaster';
+    $path = '/public_html/imgTransfer/';
+    $remotepathTodo = 'shootImport/newage2/topublish/';
+    $remotepathOriginal = '/shootImport/newage2/original/';
+    $remotepathToRename = '/shootImport/newage2/torename/';
 }
 $ftp_server_port = "21";
-$ftp_user_name = 'app@iwes.pro';
+$ftp_user_name = 'iwespro';
 $ftp_user_pass = "Cartne01!";
 $shops = \Monkey::app()->repoFactory->create('Shop')->findAll();
 
@@ -58,11 +66,47 @@ if ((!$conn_id) || (!$login_result)) {
     echo "Success</br>";
     // enabling passive mode
     ftp_pasv($conn_id,false);
+    // prendo il contenuto di tutta la directory sul server
     $buff = ftp_rawlist($conn_id, '/');
 
 // close the connection
     ftp_close($conn_id);
 
 // output the buffer
-    var_dump($buff);
+    // output $contents
+    $response = [];
+    $response ['data'] = [];
+    $i = 1;
+    $shopName = '';
+    foreach ($buff as $item) {
+        $item = trim($item,'/');
+        $item = '/' . $item;
+        if ($item === '/') {
+
+            return true;
+
+        }
+        $result = in_array($item, ftp_nlist($conn_id, dirname($item)));
+        if ($result == true) {
+            $pathArr = explode(DIRECTORY_SEPARATOR,$item);
+            $filenametoextrat = end($pathArr);
+            foreach ($shops as $shop) {
+                $shopToFind = 'shop_' . $shop->id . '_';
+                if (strpos($filenametoextrat,$shopToFind) !== false) {
+                    $shopName = $shop->name;
+                    break;
+                }
+            }
+        }
+
+
+        $row = [];
+        $row["DT_RowId"] = 'row__' . $i;
+        $row["id"] = $i;
+        $row['file'] = '<i class="fa fa-file-archive-o" aria-hidden="true"></i> '. $filenametoextrat;
+        $row['shopName']=$shopName;
+        $response['data'][] = $row;
+    }
 }
+
+return json_encode($response);
