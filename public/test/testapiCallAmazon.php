c102aa538a02f959abfcc41abe7404dc3c4860f6
juri fiorani <jurif@hotmail.com>
2021-02-08 10:24:52 +0100

    creazione Vista Vendite

--- public/test/testapiCallAmazon.php
+++ public/test/testapiCallAmazon.php
@@ -1,142 +1,47 @@
 <?php
-ini_set("memory_limit","2000M");
-ini_set('max_execution_time',0);
-$ttime = microtime(true);
-$time = microtime(true);
+
 require '../../iwesStatic.php';
-var_dump('Applicazione  \t\t\t\t' . (microtime(true) - $time));
 
-$monkey = \Monkey::app();
-$time = microtime(true);
-$monkey->dbAdapter;
-var_dump("dbAdapter \t\t\t\t\t" . (microtime(true) - $time));
-$time = microtime(true);
-$monkey->cacheService;
-var_dump("cacheService \t\t\t\t" . (microtime(true) - $time));
-$time = microtime(true);
-$monkey->sessionManager;
-var_dump("sessionManager \t\t\t\t" . (microtime(true) - $time));
-$time = microtime(true);
-$monkey->authManager;
-var_dump("authManager \t\t\t\t" . (microtime(true) - $time));
-$time = microtime(true);
-$monkey->entityManagerFactory;
-var_dump("entityManagerFactory \t\t" . (microtime(true) - $time));
-$time = microtime(true);
-$monkey->repoFactory;
-var_dump("repoFactory \t\t\t\t" . (microtime(true) - $time));
-$time = microtime(true);
-$monkey->eventManager;
-var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
-$time = microtime(true);
+//\Monkey::app()->vendorLibraries->load('amazonapi');
+
+require_once('/media/sf_sites/vendor/vendor/autoload.php');
 
 
-/*$options = array(
-    "http" => array(
-        "header" => "Content-type: text/json\r\n",
-        "method" => "POST",
-        "content" => $stringApi
-    ),
-);*/
 
+$options = [
+    'refresh_token' => 'Atzr|IwEBIFEh8rsv4QOPx-i4zw1WuEHIzTf7Mdrz9eE3uRsrqJjyTdJ1OtmhMQSSrkkws2hpTNow3yiDBZvpe8ZXwq3pheu7U0qyS7UYErcAeAxDnUq3BkATKAy5ziD5rubs4yFfD17yOx7FVyP4AgpNp50miQe-26xRNGyaHrKhCafeEjSFhWe5Msto9DW5fNQOdyAXOQOB2kYjATC6y1hn_OhVPBEjFZOG6GRphAwOK8j-jUiHZGNAfBGBRURoByW5LRhny1gxPRUmFDVjKIS20UJSA2CZEEap-cV6a8PujG7yDmBe4HxB9r_-XFcwjpi415IqN3w',
+    'client_id' => 'amzn1.application-oa2-client.1cf3ee13dbbe435caadced510a94f1f1',
+    'client_secret' => '2574f54cc10b20c7a814a2c81df7fcbd117c28ae02e3c95e42d43e04a36e4d8e',
+    'region' =>  'eu-south-1',
+    'access_key' => 'AKIAWP2CM7DXFLQPFIUR',
+    'secret_key' => ',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',
+    'endpoint' =>  'https://sts.eu-south-1.amazonaws.com',
+    'role_arn' => 'arn:aws:iam::446278990062:role/PartnerRoleAPI',
+];
 
-//Initiate cURL.
-/*
-$urlInsert = "https://api.amazon.com/auth/o2/token";
-$data = array("grant_type" => "refresh_token","refresh_token" => "Atzr|IwEBIFEh8rsv4QOPx-i4zw1WuEHIzTf7Mdrz9eE3uRsrqJjyTdJ1OtmhMQSSrkkws2hpTNow3yiDBZvpe8ZXwq3pheu7U0qyS7UYErcAeAxDnUq3BkATKAy5ziD5rubs4yFfD17yOx7FVyP4AgpNp50miQe-26xRNGyaHrKhCafeEjSFhWe5Msto9DW5fNQOdyAXOQOB2kYjATC6y1hn_OhVPBEjFZOG6GRphAwOK8j-jUiHZGNAfBGBRURoByW5LRhny1gxPRUmFDVjKIS20UJSA2CZEEap-cV6a8PujG7yDmBe4HxB9r_-XFcwjpi415IqN3w","client_id"=>"amzn1.application-oa2-client.1cf3ee13dbbe435caadced510a94f1f1","client_secret" => "2574f54cc10b20c7a814a2c81df7fcbd117c28ae02e3c95e42d43e04a36e4d8e");
-
-$postdata = json_encode($data);
-
-$ch = curl_init($urlInsert);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-$result = curl_exec($ch);
-curl_close($ch);
-$res=json_decode($result);
-$nextToken= $res->access_token;
-*/
-$urlInsert = "https://api.amazon.com/auth/o2/token";
-$data = array("grant_type" => "client_credentials","scope" => "sellingpartnerapi::notifications","client_id" => "amzn1.application-oa2-client.1cf3ee13dbbe435caadced510a94f1f1","client_secret" => "2574f54cc10b20c7a814a2c81df7fcbd117c28ae02e3c95e42d43e04a36e4d8e");
-
-$postdata = json_encode($data);
-
-$ch = curl_init($urlInsert);
-curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
-curl_setopt($ch,CURLOPT_POST,1);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$postdata);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
-curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
-$result = curl_exec($ch);
-curl_close($ch);
-$res = json_decode($result);
-$nextToken = $res->access_token;
-echo $nextToken;
-
-/*
-$urlToken = "https://sts.amazonaws.com";
-$dataToken = array("Version" => "2011-06-15",
-    "Action" => "AssumeRole",
-    "RoleArn" => "arn:aws:iam::446278990062:role/PartnerRoleAPI",
-    "PolicyArns.member.1.arn" => "arn:aws:iam::446278990062:policy/SellingPartnerAPI",
-    "Policy" => array("Version" => 2012 - 10 - 17,
-                     "Statement" => array("Sid" => "VisualEditor0","Effect" => "Allow","Action" => "sts.*","Resource" => "*")),
-    "DurationSeconds" => 3600);
-
-$postdataToken = json_encode($dataToken);
-
-$ch = curl_init($urlToken);
-$userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2';
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_PORT,443);
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,true);
-curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$postdataToken);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
-curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
-curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
-$response2 = curl_exec($ch);
-var_dump($response2);
-curl_close($ch);
-
-$urlOrder = "https://sellingpartnerapi-eu.amazon.com/orders/v0/orders";
-$dataOrder = array(
-    "NextToken" => $nextToken,
-    "MarketplaceId" => 'APJ6JRA9NG5V4',
-    "OrderStatuses" => 'Canceled'
-
+$accessToken =   \clouSale\AmazonSellingPartnerAPI\SellingPartnerOAuth::getAccessTokenFromRefreshToken(
+    $options['refresh_token'],
+    $options['client_id'],
+    $options['client_secret']
 );
-$url = 'https://sellingpartnerapi-eu.amazon.com/orders/v0/orders?MarketplaceIds=APJ6JRA9NG5V4&CreatedAfter=2018-05-10&OrderStatuses=Canceled';
-
-$headers = array(
-    'x-amz-access-token' => $nextToken,
-
+$assumedRole =   \clouSale\AmazonSellingPartnerAPI\AssumeRole::assume(
+    $options['region'],
+    $options['access_key'],
+    $options['secret_key'],
+    $options['role_arn']
 );
+$config =   \clouSale\AmazonSellingPartnerAPI\Configuration::getDefaultConfiguration();
+$config->setHost($options['endpoint']);
+$config->setAccessToken($accessToken);
+$config->setAccessKey($assumedRole->getAccessKeyId());
+$config->setSecretKey($assumedRole->getSecretAccessKey());
+$config->setRegion($options['region']);
+$config->setSecurityToken($assumedRole->getSessionToken());
+$apiInstance = new \clouSale\AmazonSellingPartnerAPI\Api\CatalogApi($config);
+$marketplace_id = 'APJ6JRA9NG5V4';
+$asin = 'B0002ZFTJA';
 
-
-$ch = curl_init($url);
-$userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2';
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_PORT,443);
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,true);
-curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);
-curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
-curl_setopt($ch,CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);
-
-curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
-
-$response1 = curl_exec($ch);
-
-curl_close($ch);
-
-$result = json_decode($response1,true);
-var_dump($response1);
+$result = $apiInstance->getCatalogItem($marketplace_id, $asin);
+echo $result->getPayload()->getAttributeSets()[0]->getTitle(); // Never Gonna Give You Up [Vinyl Single]
 
 
