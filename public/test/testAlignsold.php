497befe18577acc5e7157735b00e5b9143766b24
juri fiorani <jurif@hotmail.com>
2021-03-01 15:11:59 +0100

    creazione Vista Vendite

--- public/test/testAlignsold.php
+++ public/test/testAlignsold.php
@@ -33,7 +33,7 @@ $monkey->eventManager;
 var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
 $time = microtime(true);
 $sql='SELECT productId,productVariantId,shopId,dateStart,startQuantity,dateEnd,endQuantity,priceActive,SUM(soldQuantity)AS soldQuantity, SUM(netTotal) AS netTotal,`year`,`month`,`day`
-FROM ProductSizeSoldDay  where dateStart <\'2021-02-16 00:00:00\' and shopId!=51 GROUP BY `day`,`month`,`year`,productId,productVariantId,shopId Order BY dateStart asc';
+FROM ProductSizeSoldDay  where dateStart <\'2021-02-16 00:00:00\' and shopId=51 GROUP BY `day`,`month`,`year`,productId,productVariantId,shopId Order BY dateStart asc';
 $res=\Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
 $productSoldDayRepo=\Monkey::app()->repoFactory->create('ProductSoldDay');
 foreach($res as $result){
