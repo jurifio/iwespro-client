273777cd59809639d81a748024235e8ecb31e59a
juri fiorani <jurif@hotmail.com>
2021-03-23 10:05:40 +0100

    creazione Vista Vendite

--- public/test/testCrazyAmazon.php
+++ public/test/testCrazyAmazon.php
@@ -17,14 +17,16 @@ $config = [
 
     //STS: Keys of the IAM role which are needed to generate Secure Session
     // (a.k.a Secure token) for accessing and assuming the IAM role
-    'access_key' => 'AKIAWP2CM7DXFLQPFIUR',
-    'secret_key' => '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',
-    'role_arn' => 'arn:aws:iam::446278990062:role/PartnerRoleAPI' ,
+    'access_key' => 'AKIAWP2CM7DXOW6TRZXL',
+    'secret_key' => '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',
+    'role_arn' => 'arn:aws:iam::446278990062:user/PartnerRoleAPI' ,
 
     //API: Actual configuration related to the SP API :)
     'region' => 'eu-west-1',
     'host' => 'sellingpartnerapi-eu.amazon.com'
 ];
+//eu-west-1
+//AKIAWP2CM7DXFLQPFIUR
 $urlInsert = "https://api.amazon.com/auth/o2/token";
 $data = array("grant_type" => "refresh_token","refresh_token" => "Atzr|IwEBIFEh8rsv4QOPx-i4zw1WuEHIzTf7Mdrz9eE3uRsrqJjyTdJ1OtmhMQSSrkkws2hpTNow3yiDBZvpe8ZXwq3pheu7U0qyS7UYErcAeAxDnUq3BkATKAy5ziD5rubs4yFfD17yOx7FVyP4AgpNp50miQe-26xRNGyaHrKhCafeEjSFhWe5Msto9DW5fNQOdyAXOQOB2kYjATC6y1hn_OhVPBEjFZOG6GRphAwOK8j-jUiHZGNAfBGBRURoByW5LRhny1gxPRUmFDVjKIS20UJSA2CZEEap-cV6a8PujG7yDmBe4HxB9r_-XFcwjpi415IqN3w","client_id"=>"amzn1.application-oa2-client.1cf3ee13dbbe435caadced510a94f1f1","client_secret" => "2574f54cc10b20c7a814a2c81df7fcbd117c28ae02e3c95e42d43e04a36e4d8e");
 $data1 = array("grant_type" => "refresh_token","lwa_access_token" => "Atzr|IwEBIFEh8rsv4QOPx-i4zw1WuEHIzTf7Mdrz9eE3uRsrqJjyTdJ1OtmhMQSSrkkws2hpTNow3yiDBZvpe8ZXwq3pheu7U0qyS7UYErcAeAxDnUq3BkATKAy5ziD5rubs4yFfD17yOx7FVyP4AgpNp50miQe-26xRNGyaHrKhCafeEjSFhWe5Msto9DW5fNQOdyAXOQOB2kYjATC6y1hn_OhVPBEjFZOG6GRphAwOK8j-jUiHZGNAfBGBRURoByW5LRhny1gxPRUmFDVjKIS20UJSA2CZEEap-cV6a8PujG7yDmBe4HxB9r_-XFcwjpi415IqN3w","client_id"=>"amzn1.application-oa2-client.1cf3ee13dbbe435caadced510a94f1f1","client_secret" => "2574f54cc10b20c7a814a2c81df7fcbd117c28ae02e3c95e42d43e04a36e4d8e");
@@ -44,10 +46,10 @@ curl_close($ch);
 $res=json_decode($result);
 
 $nextToken= $res->access_token;
-$token='Atzr|IwEBIFEh8rsv4QOPx-i4zw1WuEHIzTf7Mdrz9eE3uRsrqJjyTdJ1OtmhMQSSrkkws2hpTNow3yiDBZvpe8ZXwq3pheu7U0qyS7UYErcAeAxDnUq3BkATKAy5ziD5rubs4yFfD17yOx7FVyP4AgpNp50miQe-26xRNGyaHrKhCafeEjSFhWe5Msto9DW5fNQOdyAXOQOB2kYjATC6y1hn_OhVPBEjFZOG6GRphAwOK8j-jUiHZGNAfBGBRURoByW5LRhny1gxPRUmFDVjKIS20UJSA2CZEEap-cV6a8PujG7yDmBe4HxB9r_-XFcwjpi415IqN3w';
-$stsCredentials='sts:AssumeRole';
+$token='Atzr|IwEBIIShU9Sik7v3c947K12dzLh34leDHuIxIKmhGAlmzBm2Id-ucw5x4Ugpti0IU1M3oLJwBLkPi00qY-IONqll04Nr8YVVF8ZEdkVaBiUnXEizDGmIPV8aPP0A9SG3jEaE6hxNYU3QH7muWUg1pEoZI1CHWX0DAT5_BhWYhEcdPmupiMvEaioNgYkDwyZsjLcO3jIABh1M5CwLscwJT9UWv13AKjz2CtbpVutOnWR1Aj8eVoJvghW6HZmgcwL-vhcL9aqAfV-9TYJNwlqOhtRW3nUPdRTY0iURX7Me1y8HF8CfVq2yOwCg-ZWGT_2Z0wWhPCU';
+$stsCredentials='AKIAWP2CM7DXOW6TRZXL,l5lWa7yTLurvmqdeHEhv5Sa1HzH84nCPfLCVDyji';
 $arraySalt=[];
-$arraySalt=['lwa_access_token'=>[$token,'expiresOn'=>$res->expires_in],'sts_credentials'=>[$stsCredentials,'expiresOn'=>$res->expires_in]];
+$arraySalt=['lwa_access_token'=>[$token,'expiresOn'=>$res->expires_in],'sts_credentials'=>['access_key'=>'AKIAWP2CM7DXOW6TRZXL','secret_key'=>'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','expiresOn'=>$res->expires_in,'token'=>'Atzr|IwEBIIShU9Sik7v3c947K12dzLh34leDHuIxIKmhGAlmzBm2Id-ucw5x4Ugpti0IU1M3oLJwBLkPi00qY-IONqll04Nr8YVVF8ZEdkVaBiUnXEizDGmIPV8aPP0A9SG3jEaE6hxNYU3QH7muWUg1pEoZI1CHWX0DAT5_BhWYhEcdPmupiMvEaioNgYkDwyZsjLcO3jIABh1M5CwLscwJT9UWv13AKjz2CtbpVutOnWR1Aj8eVoJvghW6HZmgcwL-vhcL9aqAfV-9TYJNwlqOhtRW3nUPdRTY0iURX7Me1y8HF8CfVq2yOwCg-ZWGT_2Z0wWhPCU']];
 
 $fp = fopen('/media/sf_sites/iwespro/aws-token/aws-token', 'w');
 file_put_contents('/media/sf_sites/iwespro/aws-token/aws-token', json_encode($arraySalt));
