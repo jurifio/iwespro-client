999f8b317051a1b216883c998f4ecc71be5235ac
juri fiorani <jurif@hotmail.com>
2021-06-24 19:12:37 +0200

    creazione Vista Vendite

---
+++ public/test/getStoreHouse.php
@@ -0,0 +1,51 @@
+<?php
+
+require '../../iwesStatic.php';
+$shootingBookingRepo = \Monkey::app()->repoFactory->create('ShootingBooking');
+$shootingRepo = \Monkey::app()->repoFactory->create('Shooting');
+$shootingDocumentRepo = \Monkey::app()->repoFactory->create('Document');
+$userRepo = \Monkey::app()->repoFactory->create('User');
+$productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
+$productSizeRepo = \Monkey::app()->repoFactory->create('ProductSize');
+$productRepo = \Monkey::app()->repoFactory->create('Product');
+$productBrandRepo=\Monkey::app()->repoFactory->create('ProductBrand');
+if ($_GET['email']) {
+    $email = $_GET['email'];
+}
+
+
+
+$user = $userRepo->findOneBy(['email' => $email]);
+$resShop = \Monkey::app()->dbAdapter->query('select shopId as shopId from UserHasShop where userId=' . $user->id,[])->fetchAll();
+foreach ($resShop as $shopResult) {
+    $shopId = $shopResult['shopId'];
+}
+
+
+
+$sql=" SELECT  concat(`st`.`name`,'-',`st`.`id`) as storehouse 
+               
+               from Storehouse st 
+                 where  `st`.`shopId`=".$shopId."   Order BY st.id asc";
+
+
+
+$resultProduct = \Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
+$datone = [];
+$dateTwo=[];
+$i = 0;
+$success=0;
+foreach ($resultProduct as $res) {
+    $datone[] = ['storeHouse' => $res['storehouse']];
+    $success=1;
+    $i++;
+}
+$dateTwo[]=['success'=>$success,'Name'=>$datone];
+
+
+
+
+
+
+echo  json_encode($dateTwo);
+
