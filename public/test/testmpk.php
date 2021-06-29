471c6a35e2cec318dac525125d4cbf73b28d8cfe
juri fiorani <jurif@hotmail.com>
2021-06-29 17:32:17 +0200

    creazione Vista Vendite

--- public/test/testmpk.php
+++ public/test/testmpk.php
@@ -3,7 +3,7 @@ require '../../iwesStatic.php';
 
 
 
-
+/*
 
 $get_data = callAPI('GET', 'https://testing.efashion.cloud/api/v3.0/products/condensed?storeCode=ASAHP', false);
 $response = json_decode($get_data, true);
@@ -46,5 +46,72 @@ function callAPI($method, $url, $data){
     if(!$result){die("Connection Failure");}
     curl_close($curl);
     return $result;
-}
-?>
+}*/
+
+$order ='{
+        "order_number" : "19558912",
+        "date":"2021-06-29",
+        "items_count":"1",
+        "items" : [
+            {
+            "product":"5938005afd7955c0bff4caa8",
+            "quantity":"1",
+            "size":"9.5",
+            "purchase_price":"282"
+            }
+       ]
+      }      
+        ';
+/*$order1 = [
+    [
+        "order_number" => '1955891',
+        "date"=>"2021-06-29",
+        "items_count"=>"1",
+        "items" => [
+            "product"=>"5938005afd7955c0bff4caa8",
+            "quantity"=>"1",
+            "size"=>"9.5",
+            "purchase_price"=>"282.00"
+        ]
+    ]
+];
+$send=json_encode($order1);
+$addOrderUrl = "https://testing.efashion.cloud/api/v3.0/place/order.json?storeCode=ASAHP";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $addOrderUrl);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $order);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods: POST',
+    'Access-Control-Allow-Origin: *',
+    'Content-type: application/x-www-form-urlencoded\r\n'
+]);
+$result = curl_exec($ch);
+$e = curl_error($ch);
+curl_close($ch);
+echo $result;
+*/
+$url = "https://testing.efashion.cloud/api/v3.0/place/order.json?storeCode=ASAHP";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+    "Content-Type: application/x-www-form-urlencoded",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = "order=".$order;
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
