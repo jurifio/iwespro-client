d2bf9f6f5ea741b736528353c2724f2df999af31
juri fiorani <jurif@hotmail.com>
2021-07-10 21:13:01 +0200

    creazione Vista Vendite

---
+++ public/test/testapiAddPrestaCron.php
@@ -0,0 +1,101 @@
+<?php
+require '../../iwesStatic.php';
+$xml='<?xml version="1.0" encoding="utf-8"?><AddFixedPriceItem xmlns="urn:ebay:apis:eBLBaseComponents"><ErrorLanguage>it_IT</ErrorLanguage><WarningLevel>High</WarningLevel><Item><AutoPay>false</AutoPay><Country>IT</Country><Currency>EUR</Currency><ListingDuration>GTC</ListingDuration><ListingType>FixedPriceItem</ListingType><PostalCode>62012</PostalCode><Location>Civitanova Marche</Location><BestOfferDetails><BestOfferEnabled>False</BestOfferEnabled></BestOfferDetails><PrimaryCategory><CategoryID>11498</CategoryID></PrimaryCategory><HitCounter>RetroStyle</HitCounter><Variations><VariationSpecificsSet><NameValueList><Name>Taglia</Name><Value>40</Value><Value>41</Value><Value>42</Value><Value>44</Value></NameValueList><NameValueList><Name>Color</Name><Value>cuoio</Value></NameValueList></VariationSpecificsSet><Variation><SKU>40399-6240181-21</SKU><StartPrice>159.00</StartPrice><Quantity>1</Quantity><VariationProductListingDetails><EAN>n/a</EAN><UPC>Non applicabile</UPC><ProductReferenceID><![CDATA[40399-6240181-21]]></ProductReferenceID></VariationProductListingDetails><VariationSpecifics><NameValueList><Name>Taglia</Name><Value>40</Value></NameValueList><NameValueList><Name>Color</Name><Value>cuoio</Value></NameValueList></VariationSpecifics></Variation><Variation><SKU>40399-6240181-23</SKU><StartPrice>159.00</StartPrice><Quantity>1</Quantity><VariationProductListingDetails><EAN>n/a</EAN><UPC>Non applicabile</UPC><ProductReferenceID><![CDATA[40399-6240181-23]]></ProductReferenceID></VariationProductListingDetails><VariationSpecifics><NameValueList><Name>Taglia</Name><Value>41</Value></NameValueList><NameValueList><Name>Color</Name><Value>cuoio</Value></NameValueList></VariationSpecifics></Variation><Variation><SKU>40399-6240181-25</SKU><StartPrice>159.00</StartPrice><Quantity>1</Quantity><VariationProductListingDetails><EAN>n/a</EAN><UPC>Non applicabile</UPC><ProductReferenceID><![CDATA[40399-6240181-25]]></ProductReferenceID></VariationProductListingDetails><VariationSpecifics><NameValueList><Name>Taglia</Name><Value>42</Value></NameValueList><NameValueList><Name>Color</Name><Value>cuoio</Value></NameValueList></VariationSpecifics></Variation><Variation><SKU>40399-6240181-69</SKU><StartPrice>159.00</StartPrice><Quantity>1</Quantity><VariationProductListingDetails><EAN>n/a</EAN><UPC>Non applicabile</UPC><ProductReferenceID><![CDATA[40399-6240181-69]]></ProductReferenceID></VariationProductListingDetails><VariationSpecifics><NameValueList><Name>Taglia</Name><Value>44</Value></NameValueList><NameValueList><Name>Color</Name><Value>cuoio</Value></NameValueList></VariationSpecifics></Variation></Variations><PictureDetails><PictureURL>https://cdn.iwes.it/pawelk-s/40399-6240181-001-1124.jpg</PictureURL><PictureURL>https://cdn.iwes.it/pawelk-s/40399-6240181-002-1124.jpg</PictureURL><PictureURL>https://cdn.iwes.it/pawelk-s/40399-6240181-003-1124.jpg</PictureURL><PictureURL>https://cdn.iwes.it/pawelk-s/40399-6240181-004-1124.jpg</PictureURL></PictureDetails><ItemSpecifics><NameValueList><Name><![CDATA[MPN]]></Name><Value><![CDATA[40399-6240181-69]]></Value></NameValueList><NameValueList><Name><![CDATA[Marca]]></Name><Value><![CDATA[Pawelk\'s]]></Value></NameValueList><NameValueList>
+        <Name><![CDATA[Materiale Tomaia]]></Name>
+        <Value><![CDATA[non applicabile]]></Value>
+      </NameValueList><NameValueList>
+        <Name><![CDATA[Larghezza della scarpa]]></Name>
+        <Value><![CDATA[non applicabile]]></Value>
+      </NameValueList>
+      <NameValueList>
+        <Name><![CDATA[Tipo]]></Name>
+        <Value><![CDATA[non applicabile]]></Value>
+      </NameValueList>
+       <NameValueList>
+        <Name><![CDATA[Modello]]></Name>
+        <Value><![CDATA[non applicabile]]></Value>
+      </NameValueList>
+        <NameValueList>
+        <Name><![CDATA[Reparto]]></Name>
+        <Value><![CDATA[non applicabile]]></Value>
+      </NameValueList>
+        <NameValueList>
+        <Name><![CDATA[Stile]]></Name>
+        <Value><![CDATA[non applicabile]]></Value>
+      </NameValueList><NameValueList><Name><![CDATA[Vintage]]></Name><Value><![CDATA[non applicabile]]></Value></NameValueList></ItemSpecifics><ConditionID>1000</ConditionID><Title><![CDATA[Stivaletti Pawelk\'s 15325 cuoio]]></Title><Description><![CDATA[Stivaletti Pawelk\'s 15325 cuoio]]>
+    </Description><Storefront><StoreCategoryID>4895391011</StoreCategoryID></Storefront><PostalCode>62012</PostalCode><Location>Civitanova Marche</Location><DispatchTimeMax>2</DispatchTimeMax><PaymentMethods>PayPal</PaymentMethods><PayPalEmailAddress>transazioni@cartechinishop.com</PayPalEmailAddress><SellerProfiles><SellerPaymentProfile><PaymentProfileID>142598637016</PaymentProfileID> <PaymentProfileName>PayPal:Bonifico bancario accettato:Copy (3</PaymentProfileName></SellerPaymentProfile> <SellerReturnProfile> 
+         <ReturnProfileID>147965984016</ReturnProfileID>
+         <ReturnProfileName>Restituzione accettata,Acquirente,30 giorni</ReturnProfileName>
+        </SellerReturnProfile> 
+        <SellerShippingProfile> 
+         <ShippingProfileID>140157110016</ShippingProfileID>
+         <ShippingProfileName>Tariffa fissa:Altro corriere Gratis/UE 10/EXUE 40</ShippingProfileName>
+        </SellerShippingProfile> 
+        </SellerProfiles> <Site>Italy</Site></Item><RequesterCredentials>
+    <eBayAuthToken>v^1.1#i^1#r^1#p^3#I^3#f^0#t^Ul4xMF8xOjk3MDVGODY4MkI3QUI4QkZGNzlGRTAwMjQwMjk4NkI4XzBfMSNFXjI2MA==</eBayAuthToken>
+  </RequesterCredentials>
+  <WarningLevel>High</WarningLevel>
+</AddFixedPriceItem>';
+$time = microtime(true);
+$devID = '9c29584f-1f9e-4c60-94dc-84f786d8670e';
+$appID = 'VendiloS-c310-4f4c-88a9-27362c05ea78';
+$certID = '3050bb00-db24-4842-999c-b943deb09d1a';
+$siteID = 101;
+
+$apiUrl = 'https://api.ebay.com/ws/api.dll';
+$apiCall = 'AddFixedPriceItem';
+$compatibilityLevel = 741;
+
+$runame = 'Vendilo_SpA-VendiloS-c310-4-prlqnbrjv';
+$loginURL = 'https://signin.ebay.it/ws/eBayISAPI.dll';
+
+$headers = array(
+    // Regulates versioning of the XML interface for the API
+    'X-EBAY-API-COMPATIBILITY-LEVEL: ' . $compatibilityLevel,
+    // Set the keys
+    'X-EBAY-API-DEV-NAME: ' . $devID,
+    'X-EBAY-API-APP-NAME: ' . $appID,
+    'X-EBAY-API-CERT-NAME: ' . $certID,
+    // The name of the call we are requesting
+    'X-EBAY-API-CALL-NAME: ' . $apiCall,
+    // SiteID must also be set in the Request's XML
+    // SiteID = 0 (US) - UK = 3, Canada = 2, Australia = 15, ....
+    // SiteID Indicates the eBay site to associate the call with
+    'X-EBAY-API-SITEID: ' . $siteID
+);
+$refMarketplaceId = '';
+$resultCall = 0;
+
+    $connection = curl_init();
+    curl_setopt($connection,CURLOPT_URL,$apiUrl);
+
+    curl_setopt($connection,CURLINFO_HEADER_OUT,true);
+// Stop CURL from verifying the peer's certificate
+
+
+// Set the headers (Different headers depending on the api call !)
+
+    curl_setopt($connection,CURLOPT_HTTPHEADER,$headers);
+
+    curl_setopt($connection,CURLOPT_POST,1);
+
+// Set the XML body of the request
+    curl_setopt($connection,CURLOPT_POSTFIELDS,$xml);
+
+// Set it to return the transfer as a string from curl_exec
+    curl_setopt($connection,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($connection,CURLOPT_SSL_VERIFYPEER,0);
+    curl_setopt($connection,CURLOPT_SSL_VERIFYHOST,0);
+// Send the Request
+    $response = curl_exec($connection);
+    //$xmlresponse = new \SimpleXMLElement($response);
+
+
+
+
+    $responseNewProduct = new \SimpleXMLElement($response);
+    $refMarketplaceId = $responseNewProduct->ItemID;
+if ($responseNewProduct->Ack == 'Success') {
+    echo $refMarketplaceId;
+}
+ ?>
