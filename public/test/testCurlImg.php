3f3ecea6475932abf2a412ea661669059a7af532
juri fiorani <jurif@hotmail.com>
2021-07-20 23:54:32 +0200

    creazione Vista Vendite

---
+++ public/test/testCurlImg.php
@@ -0,0 +1,41 @@
+<?php
+
+use bamboo\core\exceptions\BambooException;
+
+ini_set("memory_limit", "2000M");
+ini_set('max_execution_time', 0);
+$ttime = microtime(true);
+$time = microtime(true);
+require '../../iwesStatic.php';
+var_dump('Applicazione  \t\t\t\t' . (microtime(true) - $time));
+
+$monkey = \Monkey::app();
+$time = microtime(true);
+$monkey->dbAdapter;
+var_dump("dbAdapter \t\t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->cacheService;
+var_dump("cacheService \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->sessionManager;
+var_dump("sessionManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->authManager;
+var_dump("authManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->entityManagerFactory;
+var_dump("entityManagerFactory \t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->repoFactory;
+var_dump("repoFactory \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->eventManager;
+var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$url='https://s3-eu-west-1.amazonaws.com/dockint-images/sheet/DP066 FS0192D XXX PDD999Black_3.jpg';
+$c = curl_init();
+curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($c, CURLOPT_URL, htmlspecialchars_decode(str_replace(' ','%20',$url)));
+$imgBody = curl_exec($c);
+curl_close($c);
+
