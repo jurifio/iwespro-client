e4d7db12d3cd508616afdafecb6d8a42cdcbd45a
juri fiorani <jurif@hotmail.com>
2021-11-22 16:42:54 +0100

    creazione Vista Vendite

--- public/test/alignChecksumDirtySku.php
+++ public/test/alignChecksumDirtySku.php
@@ -9,6 +9,7 @@ try {
     $dsRepo=\Monkey::app()->repoFactory->create('DirtySku');
     $sql='SELECT id,dirtyProductId,shopId,size,extSkuId,qty,price,`value`,barcode,storeHouseId from DirtySku where `checksum` is null and shopId=61';
     $res=\Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
+    $cheksumUpdate=[];
     foreach($res as $rawDirtySku) {
         $dirtySku = [];
         $dirtySku['dirtyProductId'] = $rawDirtySku['dirtyProductId'];
@@ -21,12 +22,13 @@ try {
         $dirtySku['barcode']=$rawDirtySku['barcode'];
         $dirtySku['storeHouseId']=1;
         $checksum=md5(json_encode($dirtySku));
-        $ds=$dsRepo->findOneBy(['id'=>$rawDirtySku['id']]);
-        $ds->checksum=$checksum;
+        $checksumUpdate[]=['id'=>$rawDirtySku['id'],'checksum'=>$checksum];
+
+    }
+    foreach ($checksumUpdate as $check){
+        $ds=$dsRepo->findOneBy(['id'=>$check['id']]);
+        $ds->checksum=$check['checksum'];
         $ds->update();
-
-
-
     }
 
 } catch (\Throwable $e) {
