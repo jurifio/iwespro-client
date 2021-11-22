e9aaae41039e0726f5e922dc52e69ea090c03394
juri fiorani <jurif@hotmail.com>
2021-11-22 16:28:01 +0100

    creazione Vista Vendite

---
+++ public/test/alignChecksumDirtySku.php
@@ -0,0 +1,36 @@
+<?php
+
+use bamboo\core\exceptions\BambooException;
+
+require '../../iwesStatic.php';
+
+
+try {
+    $dsRepo=\Monkey::app()->repoFactory->create('DirtySku');
+    $sql='SELECT id,dirtyProductId,shopId,size,extSkuId,qty,price,`value`,barcode,storeHouseId from DirtySku where `checksum` is null and shp.shopId=61';
+    $res=\Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
+    foreach($res as $rawDirtySku) {
+        $dirtySku = [];
+        $dirtySku['dirtyProductId'] = $rawDirtySku['dirtyProductId'];
+        $dirtySku['shopId'] = $rawDirtySku['shopId'];
+        $dirtySku['size'] = $rawDirtySku['size'];
+        $dirtySku['extSkuId'] = $rawDirtySku['extSkuId'];
+        $dirtySku['qty'] = $rawDirtySku['qty'];
+        $dirtySku['price'] = $rawDirtySku['price'];
+        $dirtySku['value'] = $rawDirtySku['value'];
+        $dirtySku['barcode']=$rawDirtySku['barcode'];
+        $dirtySku['storeHouseId']=1;
+        $checksum=md5(json_encode($dirtySku));
+        $ds=$dsRepo->findOneBy(['id'=>$rawDirtySku['id']]);
+        $ds->checksum=$checksum;
+        $ds->update();
+
+
+
+    }
+
+} catch (\Throwable $e) {
+     echo $e->getMessage();
+}
+
+
