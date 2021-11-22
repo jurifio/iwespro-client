386c029fde904b4a1207d36b2eab5fba5d15d857
juri fiorani <jurif@hotmail.com>
2021-11-22 17:44:03 +0100

    creazione Vista Vendite

--- public/test/alignChecksumDirtySku.php
+++ public/test/alignChecksumDirtySku.php
@@ -6,30 +6,28 @@ require '../../iwesStatic.php';
 
 
 try {
-    $dsRepo=\Monkey::app()->repoFactory->create('DirtySku');
-    $sql='SELECT id,dirtyProductId,shopId,size,extSkuId,qty,price,`value`,barcode,storeHouseId from DirtySku where `checksum` is null and shopId=61';
-    $res=\Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
-    $cheksumUpdate=[];
-    foreach($res as $rawDirtySku) {
+    $ds=\Monkey::app()->repoFactory->create('DirtySku')->findBy(['shopId'=>61]);
+    foreach($ds as $sku){
+        if(is_null($sku->checksum)){
         $dirtySku = [];
-        $dirtySku['dirtyProductId'] = $rawDirtySku['dirtyProductId'];
-        $dirtySku['shopId'] = $rawDirtySku['shopId'];
-        $dirtySku['size'] = $rawDirtySku['size'];
-        $dirtySku['extSkuId'] = $rawDirtySku['extSkuId'];
-        $dirtySku['qty'] = $rawDirtySku['qty'];
-        $dirtySku['price'] = $rawDirtySku['price'];
-        $dirtySku['value'] = $rawDirtySku['value'];
-        $dirtySku['barcode']=$rawDirtySku['barcode'];
-        $dirtySku['storeHouseId']=1;
+        $dirtySku['dirtyProductId'] = $sku->dirtyProductId;
+        $dirtySku['shopId'] = $sku->shopId;
+        $dirtySku['size'] = $sku->size;
+        $dirtySku['extSkuId'] = $sku->extSkuId;
+        $dirtySku['qty'] = $sku->qty;
+        $dirtySku['price'] = $sku->price;
+        $dirtySku['value'] = $sku->value;
+        $dirtySku['barcode']=$sku->barcode;
+        $dirtySku['storeHouseId']=$sku->storeHouseId;
         $checksum=md5(json_encode($dirtySku));
-        $checksumUpdate[]=['id'=>$rawDirtySku['id'],'checksum'=>$checksum];
+        $sku->checksum=$checksum;
+        $sku->update();
+        echo 'eseguito linea ' .$sku->id. ' '.$sku->size.' ' .$sku->dirtyProductId;
+        } else{
+            continue;
+        }
+    }
 
-    }
-    foreach ($checksumUpdate as $check){
-        $ds=$dsRepo->findOneBy(['id'=>$check['id']]);
-        $ds->checksum=$check['checksum'];
-        $ds->update();
-    }
 
 } catch (\Throwable $e) {
      echo $e->getMessage();
