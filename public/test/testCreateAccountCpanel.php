c42e7bafac359b89b22daa1b63ae47f0c26c0021
juri fiorani <jurif@hotmail.com>
2022-04-22 20:06:58 +0200

    creazione Vista Vendite

--- public/test/testCreateAccountCpanel.php
+++ public/test/testCreateAccountCpanel.php
@@ -6,9 +6,13 @@ use bamboo\core\utils\cpanel\createCPanelAccount;
 require '../../iwesStatic.php';
 
 try {
+    $newdomain=$_GET['domain'];
+    $newUser=$_GET['newUser'];
+    $newPass=$_GET['newPass'];
+    $newEmail=$_GET['newEmail'];
      $createAccount =   new createCPanelAccount();
-     $result=$createAccount->createAccount('testcreate.iwes.it','newtest','Newpass57867?','default','juri@iwes.it','root','F1fiI3EYv9JXl8Z','front.iwes.it');
-
+    // $result=$createAccount->createAccount('testcreate.iwes.it','newtest','Newpass57867?','default','juri@iwes.it','root','F1fiI3EYv9JXl8Z','front.iwes.it');
+    $result=$createAccount->createAccount($newdomain,$newUser,$newPass,'default',$newEmail,'root','F1fiI3EYv9JXl8Z','front.iwes.it');
     header('Location:' .$result);
 }
 catch (Exception $e) {
