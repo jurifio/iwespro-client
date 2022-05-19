e01de8e5c23eb9dc5c5af3fe49fdd2a0a03c4761
juri fiorani <jurif@hotmail.com>
2022-05-19 17:32:46 +0200

    creazione Vista Vendite

--- public/test/testCreateJsonSetup.php
+++ public/test/testCreateJsonSetup.php
@@ -2,6 +2,7 @@
 
 use bamboo\core\exceptions\BambooException;
 use bamboo\core\utils\cpanel\createCPanelAccount;
+use bamboo\core\utils\sftp\sftpClient;
 
 require '../../iwesStatic.php';
 
@@ -33,6 +34,33 @@ try {
     fclose($myfile);
     $extract=json_decode(file_get_contents($nameLocalFile),false);
     echo $extract->urlSite;
+    $indexFile='index.php';
+    if(ENV=='dev') {
+        $myIndexfile = fopen("/media/sf_sites/iwespro/temp/" . $indexFile,"w");
+        $nameIndexFile="/media/sf_sites/iwespro/temp/" . $indexFile;
+    }else{
+        $myIndexfile = fopen("/home/iwespro/public_html/temp/" . $indexFile,"w");
+        $nameIndexFile="/home/iwespro/public_html/temp/" . $indexFile;
+    }
+    $codesetup="<?php
+        mkdir('/home/demo2iwes/public_html/tempInstall', 0777,true);
+copy('/home/shared/setup/preinstall.zip','/home/demo2iwes/public_html/preinstall.zip');
+\$zip = new ZipArchive;
+if (\$zip->open('preinstall.zip') === TRUE) {
+    \$zip->extractTo('/home/demo2iwes/public_html/');
+    \$zip->close();
+    echo 'ok';
+} else {
+    echo 'failed';
+}
+?>
+<a href='/setup/dist/index.php'>installa</a>";
+    fwrite($myIndexfile, $codesetup);
+
+    fclose($myIndexfile);
+
+
+
 }
 
 catch (Exception $e) {
