b9f6bf123d99ce4564bb97fa48400192ed6c8b49
jurif <jurif@hotmail.com>
2025-06-18 19:16:28 +0200

    aggiornamento  CAuthmanager

---
+++ public/test/testjob.php
@@ -0,0 +1,45 @@
+<?php
+$ttime = microtime(true);
+$time = microtime(true);
+require '../../iwesStatic.php';
+var_dump('Applicazione  \t\t\t\t' . (microtime(true) - $time));
+
+$monkey = \Monkey::app();
+$time = microtime(true);
+$monkey->dbAdapter;
+var_dump("dbAdapter \t\t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->cacheService;
+var_dump("cacheService \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->sessionManager;
+var_dump("sessionManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->authManager;
+var_dump("authManager \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->entityManagerFactory;
+var_dump("entityManagerFactory \t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->repoFactory;
+var_dump("repoFactory \t\t\t\t" . (microtime(true) - $time));
+$time = microtime(true);
+$monkey->eventManager;
+var_dump("eventManager \t\t\t\t" . (microtime(true) - $time));
+
+
+
+
+try {
+
+    $sql='update `Job` set manualStart=1 where id=74';
+    \Monkey::app()->dbAdapter->query($sql,[]);
+
+
+
+
+} catch (\Throwable $e) {
+     echo $e->getMessage();
+}
+
+
